90048eb57241f14078cbb9ebb8d068d4161620cb
ds-ext-jserwa <104501924+ds-ext-jserwa@users.noreply.github.com>
2022-06-23 14:07:21 +0100

    TRACEFOSS-337: Add cors configuration (#25)
    * TRACEFOSS-337: Add cors configuration

--- src/main/java/net/catenax/traceability/config/SecurityConfig.java
+++ src/main/java/net/catenax/traceability/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,6 +13,11 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @KeycloakConfiguration
 @ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true", matchIfMissing = true)
@@ -35,9 +41,25 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		super.configure(http);
 
-		http.authorizeRequests()
+		http
+			.cors()
+			.and()
+			.authorizeRequests()
 			.antMatchers(WHITELIST_URLS).permitAll()
-			.antMatchers("/api/*").authenticated();
+			.antMatchers("/api/**").authenticated();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource(@Value("${cors.origins}") List<String> origins) {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(origins);
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setAllowedHeaders(List.of("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
 	}
 
 	@Bean
