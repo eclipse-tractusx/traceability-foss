b633ca302cd8964e80f2e247be2398c4274a9ec6
Blazej Kepa <blazej.kepa@partner.doubleslash.de>
2022-11-08 11:52:28 +0100

    Pipeline fix attempt 14

---
+++ .github/scripts/eclipse_dash.sh
@@ -0,0 +1,3 @@
+./gradlew dependencies | ggrep -Poh "(?<=\s)[\w\.-]+:[\w\.-]+:[^:\s]+" | grep -v "^org\.eclipse" | sort | uniq > DEPENDENCIES
+          java -jar org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar DEPENDENCIES -project automotive.tractusx -summary DASH_SUMMARY
+          grep "restricted" DASH_SUMMARY
