2bfc3beeaa3a3e491edbcb84cc3881209c58bdf3
mk <martin.kanal@doubleslash.de>
2022-12-02 12:26:28 +0100

    fix(cdcd):[TRACEFOSS-762] elipse ip dash check improvements

--- .github/scripts/eclipse_dash.sh
+++ .github/scripts/eclipse_dash.sh
@@ -1,3 +1,3 @@
-./gradlew dependencies | grep -Poh "(?<=\s)[\w\.-]+:[\w\.-]+:[^:\s]+" | grep -v "^org\.eclipse" | sort | uniq > DEPENDENCIES
-java -jar org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar DEPENDENCIES -project automotive.tractusx -summary DASH_SUMMARY
-grep "restricted" DASH_SUMMARY | if test $(wc -l) -gt 0; then exit 1; fi
+./gradlew dependencies | grep -Poh "(?<=\s)[\w\.-]+:[\w\.-]+:[^:\s]+" | grep -v "^org\.eclipse" | sort | uniq > DASH_SUMMARY
+java -jar org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar DASH_SUMMARY -project automotive.tractusx -summary DEPENDENCIES
+grep "restricted" DEPENDENCIES | if test $(wc -l) -gt 0; then exit 1; fi
