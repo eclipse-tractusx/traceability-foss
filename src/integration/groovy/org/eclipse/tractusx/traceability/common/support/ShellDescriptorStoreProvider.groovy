92f8fee37b2946eaf03f1e5da8572c1c81480c82
Gregor Stelzer <gregor.stelzer@msg-david.de>
2022-12-05 13:20:51 +0100

    TRACEFOSS-762 change header

--- src/integration/groovy/org/eclipse/tractusx/traceability/common/support/ShellDescriptorStoreProvider.groovy
+++ src/integration/groovy/org/eclipse/tractusx/traceability/common/support/ShellDescriptorStoreProvider.groovy
@@ -18,6 +18,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
+
 package org.eclipse.tractusx.traceability.common.support
 
 
