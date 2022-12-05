ecd69fcaea40864db11809c860d9b82516f86daf
Gregor Stelzer <gregor.stelzer@msg-david.de>
2022-12-05 09:12:47 +0100

    ftr: change header for eclipse movement
    [TRACEFOSS-763]

--- src/app/modules/shared/components/select/select.component.scss
+++ src/app/modules/shared/components/select/select.component.scss
@@ -1,8 +1,7 @@
 /********************************************************************************
- * Copyright (c) 2022,2023
- *        2022: Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
- *        2022: ZF Friedrichshafen AG
- * Copyright (c) 2022,2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2022, 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ * Copyright (c) 2022, 2023 ZF Friedrichshafen AG
+ * Copyright (c) 2022, 2023 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
