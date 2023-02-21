d978399b2fadfa0fb4c80891b968c1d4f045e796
Fabian Bedarf <fabian.bedarf@evia.de>
2023-02-21 14:48:19 +0100

    ftr: update to latest package versions
    TRACEFOSS-1023

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      // suppressAll: true, // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
