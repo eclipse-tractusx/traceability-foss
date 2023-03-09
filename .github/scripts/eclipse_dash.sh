854135926175db7fe4d39f70bc7c3a472d665a4c
Laszlo Graf <laszlo.graf@msg.group>
2023-03-09 11:59:31 +0200

    eclipse-dash.yml

--- .github/scripts/eclipse_dash.sh
+++ .github/scripts/eclipse_dash.sh
@@ -1,2 +1,2 @@
-java -jar org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar yarn.lock -project automotive.tractusx -summary DASH_SUMMARY
-grep -E '(restricted, #)|(restricted$)' DASH_SUMMARY | if test $(wc -l) -gt 0; then exit 1; fi
+java -jar org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar yarn.lock -project automotive.tractusx -summary /home/runner/work/tx-traceability-foss-frontend/tx-traceability-foss-frontend/frontend/DASH_SUMMARY
+grep -E '(restricted, #)|(restricted$)' /home/runner/work/tx-traceability-foss-frontend/tx-traceability-foss-frontend/frontend/DASH_SUMMARY | if test $(wc -l) -gt 0; then exit 1; fi
