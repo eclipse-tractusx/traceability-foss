9db0870121b2f245727296de09dfc54f1b7ac31f
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-03-22 07:12:53 +0100

    feature: TRACEFOSS-1171 Updated length of data fields for close, accept and decline reason. Added validation of update request to get validation error instead of sql error.

---
+++ backend/src/main/resources/db/migration/V10__alter_investigation_table_change_reason_field_lenght.sql
@@ -0,0 +1,9 @@
+ALTER TABLE investigation
+ALTER COLUMN accept_reason TYPE VARCHAR(1000);
+
+ALTER TABLE investigation
+ALTER COLUMN decline_reason TYPE VARCHAR(1000);
+
+ALTER TABLE investigation
+ALTER COLUMN close_reason TYPE VARCHAR(1000);
+
