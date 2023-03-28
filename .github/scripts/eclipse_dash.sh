9c7e9342dc7abbc68a74a39898cb34c89a0ba69a
Darek Skrzypczak <darek.skrzypczak@partner.doubleslash.de>
2023-03-28 17:04:11 +0200

    fix: TRACEFOSS-1285 Fix eclipse_dash.sh on FE

--- .github/scripts/eclipse_dash.sh
+++
@@ -1,2 +0,0 @@
-java -jar /home/runner/work/tx-traceability-foss-frontend/tx-traceability-foss-frontend/org.eclipse.dash.licenses-0.0.1-20221105.055038-599.jar yarn.lock -project automotive.tractusx -summary /home/runner/work/tx-traceability-foss-frontend/tx-traceability-foss-frontend/frontend/DASH_SUMMARY
-grep -E '(restricted, #)|(restricted$)' /home/runner/work/tx-traceability-foss-frontend/tx-traceability-foss-frontend/frontend/DASH_SUMMARY | if test $(wc -l) -gt 0; then exit 1; fi
