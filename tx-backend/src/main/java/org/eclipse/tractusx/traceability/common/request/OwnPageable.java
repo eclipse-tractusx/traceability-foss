0a9f514403669a4e9430b64a1e6d92973b52cda8
Martin Maul <martin.maul@doubleslash.de>
2023-05-26 16:16:46 +0200

    chore(security):[TRACEFOSS-XXX] OwnPageable nullpointer handling

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/request/OwnPageable.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/request/OwnPageable.java
@@ -4,13 +4,10 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-
-import java.util.List;
-
-import static org.apache.commons.collections4.ListUtils.emptyIfNull;
 
 @Data
 @AllArgsConstructor
@@ -18,28 +15,32 @@ public class OwnPageable {
     private Integer page;
     private Integer size;
     @ArraySchema(arraySchema = @Schema(description = "Content of Assets PageResults"), maxItems = Integer.MAX_VALUE)
-    private List<Sort> sort;
+    private String sort;
 
     public static Pageable toPageable(OwnPageable ownPageable) {
         int usedPage = 0;
         int usedPageSize = 50;
 
-        if(ownPageable.page != null) {
+        if (ownPageable.page != null) {
             usedPage = ownPageable.getPage();
         }
 
-        if(ownPageable.size != null) {
+        if (ownPageable.size != null) {
             usedPageSize = ownPageable.getSize();
         }
 
-        Sort sort = emptyIfNull(ownPageable.getSort()).stream().findFirst().orElse(Sort.unsorted());
-        return PageRequest.of(usedPage, usedPageSize, sort);
+        Sort usedSort = Sort.unsorted();
+
+        if (!StringUtils.isBlank(ownPageable.getSort())) {
+            usedSort = Sort.by(ownPageable.getSort());
+        }
+
+        return PageRequest.of(usedPage, usedPageSize, usedSort);
     }
 
     public static OwnPageable toOwnPageable(Pageable pageable) {
-        return new OwnPageable(pageable.getPageNumber(), pageable.getPageSize(), List.of(pageable.getSort()));
+        return new OwnPageable(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort().toString());
     }
-
 
 
 }
