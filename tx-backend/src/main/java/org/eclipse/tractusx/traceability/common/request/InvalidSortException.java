67a21a073c89312d888450edd45f5bfdc4c9159f
ds-ext-sceronik <sebastian.ceronik@partner.doubleslash.de>
2023-05-29 16:01:33 +0200

    fix: TRACEFOSS-1520 sort request bug

---
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/request/InvalidSortException.java
@@ -0,0 +1,9 @@
+package org.eclipse.tractusx.traceability.common.request;
+
+public class InvalidSortException extends RuntimeException {
+
+    public InvalidSortException(String message) {
+        super(message);
+    }
+
+}
