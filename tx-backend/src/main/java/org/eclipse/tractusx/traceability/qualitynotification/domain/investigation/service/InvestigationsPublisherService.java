b250deedf615bc92b73133341094497392752d2b
Martin Maul <martin.maul@doubleslash.de>
2023-06-06 08:41:00 +0200

    chore(response):[TRACEFOSS-1502] optimize error message

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/qualitynotification/domain/investigation/service/InvestigationsPublisherService.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/qualitynotification/domain/investigation/service/InvestigationsPublisherService.java
@@ -165,7 +165,7 @@ public class InvestigationsPublisherService {
                 case ACCEPTED -> investigation.accept(reason, notificationToSend);
                 case DECLINED -> investigation.decline(reason, notificationToSend);
                 case CLOSED -> investigation.close(reason, notificationToSend);
-                default -> throw new InvestigationIllegalUpdate("Transition from status %s to status %s is not allowed for investigation with id %s".formatted(investigation.getInvestigationStatus().name(), status, investigation.getInvestigationId()));
+                default -> throw new InvestigationIllegalUpdate("Transition from status \"%s\" to status \"%s\" is not allowed for investigation with id \"%s\"".formatted(investigation.getInvestigationStatus().name(), status, investigation.getInvestigationId()));
             }
             log.info("::updateInvestigationPublisher::notificationToSend {}", notificationToSend);
             investigation.addNotification(notificationToSend);
@@ -183,15 +183,15 @@ public class InvestigationsPublisherService {
         switch (status) {
             case ACKNOWLEDGED, ACCEPTED, DECLINED -> {
                 if (isInvalidAcknowledgeOrAcceptOrDecline) {
-                    throw new InvestigationIllegalUpdate("Transition from status %s to status %s is not allowed for investigation with BPN %s and application with BPN %s".formatted(investigation.getInvestigationStatus().name(), status, investigation.getBpn(), applicationBpn.value()));
+                    throw new InvestigationIllegalUpdate("Transition from status \"%s\" to status \"%s\" is not allowed for investigation with BPN \"%s\" and application with BPN \"%s\"".formatted(investigation.getInvestigationStatus().name(), status, investigation.getBpn(), applicationBpn.value()));
                 }
             }
             case CLOSED -> {
                 if (isInvalidClose) {
-                    throw new InvestigationIllegalUpdate("Transition from status %s to status %s is not allowed for investigation with BPN %s and application with BPN %s".formatted(investigation.getInvestigationStatus().name(), status, investigation.getBpn(), applicationBpn.value()));
+                    throw new InvestigationIllegalUpdate("Transition from status \"%s\" to status \"%s\" is not allowed for investigation with BPN \"%s\" and application with BPN \"%s\"".formatted(investigation.getInvestigationStatus().name(), status, investigation.getBpn(), applicationBpn.value()));
                 }
             }
-            default -> throw new InvestigationIllegalUpdate("Unknown Transition from status %s to status %s is not allowed for investigation with id %s".formatted(investigation.getInvestigationStatus().name(), status, investigation.getInvestigationId()));
+            default -> throw new InvestigationIllegalUpdate("Unknown Transition from status \"%s\" to status \"%s\" is not allowed for investigation with id \"%s\"".formatted(investigation.getInvestigationStatus().name(), status, investigation.getInvestigationId()));
         }
     }
 
@@ -209,7 +209,7 @@ public class InvestigationsPublisherService {
                     notificationGroup.clear();
                     notificationGroup.add(notification);
                 } else if (notification.getCreated().isEqual(latestNotification.get().getCreated())) {
-                    throw new IllegalArgumentException("Two notifications with same edcNotificationId have the same status. This can happen on old datasets.");
+                    throw new IllegalArgumentException("Two notifications with the same edcNotificationId have the same status. This can happen on old datasets.");
                 }
             }
             notificationMap.put(edcNotificationId, notificationGroup);
