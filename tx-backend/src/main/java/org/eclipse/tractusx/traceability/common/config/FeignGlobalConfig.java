a3c2c8be6169e74b29e5edfc2bc45c05caee2616
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-06-16 08:55:50 +0200

    feature: TRACEFOSS-442 - integrated repository

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/config/FeignGlobalConfig.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/config/FeignGlobalConfig.java
@@ -29,7 +29,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackages = {
-        "org.eclipse.tractusx.traceability.assets.infrastructure.repository.rest.irs",
+        "org.eclipse.tractusx.traceability.assets.infrastructure.base.irs",
         "org.eclipse.tractusx.traceability.shelldescriptor.infrastructure.repository.rest.registry",
         "org.eclipse.tractusx.traceability.discovery.infrastructure.repository",
 })
