0ef4c8b1a5b070ff7d194d57a7f7ce731fcdd713
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-06-19 08:04:56 +0200

    feature: TRACEFOSS-442 - integrated repository

--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/assets/infrastructure/repository/rest/irs/IrsServiceTest.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/assets/infrastructure/repository/rest/irs/IrsServiceTest.java
@@ -152,7 +152,7 @@ class IrsServiceTest {
                         new PartTypeInformation("classification", "Name at Manufacturer", "Name at Customer",
                                 "ManufacturerPartId123", "CustomerPartId123"),
                         new ManufacturingInformation("Country", new Date()),
-                        Collections.emptyList(), validityPeriod, List.of(site), "asPlanned"
+                        Collections.emptyList(), validityPeriod, List.of(site)
 
                 )
         );
