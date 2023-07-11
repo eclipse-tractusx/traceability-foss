9eac5ea8ec51d5d45cc2df4373135eae30a3475e
ds-lcapellino <lucas.capellino@doubleslash.de>
2023-07-11 14:31:09 +0200

    chore: TRACEFOSS-XXX remove unused method

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/shelldescriptor/domain/model/ShellDescriptor.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/shelldescriptor/domain/model/ShellDescriptor.java
@@ -23,14 +23,7 @@ package org.eclipse.tractusx.traceability.shelldescriptor.domain.model;
 
 import lombok.Builder;
 import lombok.Data;
-import org.eclipse.tractusx.traceability.assets.domain.model.Asset;
-import org.eclipse.tractusx.traceability.assets.domain.model.Owner;
-import org.eclipse.tractusx.traceability.assets.domain.model.QualityType;
-import org.eclipse.tractusx.traceability.assets.domain.model.SemanticDataModel;
-import org.eclipse.tractusx.traceability.assets.domain.model.SemanticModel;
 import org.springframework.util.StringUtils;
-
-import java.util.Collections;
 
 
 @Data
@@ -50,36 +43,6 @@ public class ShellDescriptor {
             return EMPTY_TEXT;
         }
         return value;
-    }
-
-
-    public Asset toAsset(String manufacturerName) {
-        String semanticModelId = null;
-        SemanticDataModel semanticDataModel = null;
-        if (getBatchId() != null) {
-            semanticModelId = getBatchId();
-            semanticDataModel = SemanticDataModel.BATCH;
-        }
-        if (getPartInstanceId() != null) {
-            semanticModelId = getPartInstanceId();
-            semanticDataModel = SemanticDataModel.SERIALPART;
-        }
-        return Asset.builder()
-                .id(getGlobalAssetId())
-                .idShort(getIdShort())
-                .semanticModel(SemanticModel.from(this))
-                .semanticModelId(semanticModelId)
-                .manufacturerId(defaultValue(getManufacturerId()))
-                .manufacturerName(manufacturerName)
-                .owner(Owner.OWN)
-                .childRelations(Collections.emptyList())
-                .parentRelations(Collections.emptyList())
-                .underInvestigation(false)
-                .activeAlert(false)
-                .qualityType(QualityType.OK)
-                .van("--")
-                .semanticDataModel(semanticDataModel)
-                .build();
     }
 
 }
