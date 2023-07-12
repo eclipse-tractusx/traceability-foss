945546f200a3bf3ac25d3536bb49febb6ce6fbff
ds-mmaul <117836305+ds-mmaul@users.noreply.github.com>
2023-07-12 13:03:19 +0200

    Chore/tracefoss xxx fix fe test coverage (#398)
    * chore(tests):[TRACEFOSS-XXX] add unit tests for alert-detail.facade.ts
    
    * chore(test):[TRACEFOSS-XXX] add unit tests for investigation-detail.facade.ts

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      clearContext: true, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
@@ -77,9 +77,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: true,
-    restartOnFileChange: true,
+    singleRun: false
   });
 };
