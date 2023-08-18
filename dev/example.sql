6ea78780d3b80b18a60f703ca9b32023c37f8616
Martin Maul <martin.maul@doubleslash.de>
2023-08-18 13:35:34 +0200

    chore(QG):[TRACEFOSS-2518] remove license header from .sql files

--- dev/example.sql
+++ dev/example.sql
@@ -1,20 +1,3 @@
-##    Copyright (c) 2023 Contributors to the Eclipse Foundation
-##
-##    See the NOTICE file(s) distributed with this work for additional
-##    information regarding copyright ownership.
-##
-##    This program and the accompanying materials are made available under the
-##    terms of the Apache License, Version 2.0 which is available at
-##    https://www.apache.org/licenses/LICENSE-2.0.
-##
-##    Unless required by applicable law or agreed to in writing, software
-##    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-##    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-##    License for the specific language governing permissions and limitations
-##    under the License.
-##
-##  SPDX-License-Identifier: Apache-2.0
-
 ## Query for assets with childs:
 
 SELECT asset.id, asset.customer_part_id,asset.id_short, asset.manufacturer_id, asset.manufacturer_name, asset.manufacturer_part_id, asset.manufacturing_country, asset.manufacturing_date, asset.name_at_customer, asset.name_at_manufacturer, asset.quality_type, asset.batch_id, asset.part_instance_id, asset.van
