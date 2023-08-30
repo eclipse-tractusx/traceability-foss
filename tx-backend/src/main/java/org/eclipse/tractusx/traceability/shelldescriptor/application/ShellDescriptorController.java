895a6377305f21a77860cb70a840dd697ac6b9d0
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-08-30 10:38:01 +0200

    chore: TRACEFOSS-2607 add response of shelldescriptor to tx-models

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/shelldescriptor/application/ShellDescriptorController.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/shelldescriptor/application/ShellDescriptorController.java
@@ -1,4 +1,22 @@
-package org.eclipse.tractusx.traceability.shelldescriptor.application.rest;
+/********************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+package org.eclipse.tractusx.traceability.shelldescriptor.application;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,11 +28,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.tractusx.traceability.common.response.ErrorResponse;
 import org.eclipse.tractusx.traceability.shelldescriptor.application.ShellDescriptorService;
+import org.eclipse.tractusx.traceability.shelldescriptor.application.mapper.ShellDescriptorResponseMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shelldescriptors.ShellDescriptorResponse;
+
+import java.util.List;
 
 @RestController
 @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SUPERVISOR', 'ROLE_USER')")
@@ -62,8 +84,8 @@ public class ShellDescriptorController {
                             mediaType = "application/json",
                             schema = @Schema(implementation = ErrorResponse.class)))})
     @GetMapping()
-    public void findAll() {
-        shellDescriptorService.findAll();
+    public List<ShellDescriptorResponse> findAll() {
+        return ShellDescriptorResponseMapper.from(shellDescriptorService.findAll());
     }
 
     @Operation(operationId = "deleteAll",
