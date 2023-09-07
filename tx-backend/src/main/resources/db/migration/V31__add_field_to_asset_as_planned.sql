8737bcbcf46285f9e2278bea6b6c7ec81cbb77df
ds-lcapellino <lucas.capellino@doubleslash.de>
2023-09-07 16:09:46 +0200

    feature: TRACEFOSS-1731 refactor flyway

--- tx-backend/src/main/resources/db/migration/V31__add_field_to_asset_as_planned.sql
+++ tx-backend/src/main/resources/db/migration/V31__add_field_to_asset_as_planned.sql
@@ -1,15 +1,20 @@
 alter table if exists assets_as_planned
-    add column validityPeriodFrom varchar (255);
+    add column validity_period_from varchar (255);
 
 alter table if exists assets_as_planned
-    add column validityPeriodTo varchar (255);
+    add column validity_period_to varchar (255);
 
 alter table if exists assets_as_planned
-    add column functionValidUntil varchar (255);
+    add column function_valid_until varchar (255);
 
 alter table if exists assets_as_planned
-    add column functionValidFrom varchar (255);
+    add column function_valid_from varchar (255);
 
 alter table if exists assets_as_planned
     add column function varchar (255);
 
+alter table if exists assets_as_planned
+    add column manufacturer_name varchar (255);
+
+alter table if exists assets_as_planned
+    add column van varchar (255);
