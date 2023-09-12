535ebc34f4190aeaedbc58dc5eb0dc15f6adfba7
ds-ext-sceronik <sebastian.ceronik@partner.doubleslash.de>
2023-09-12 21:39:21 +0200

    feature: TRACEFOSS-873 tests added

--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/assets/AssetAsBuiltControllerFilteringIT.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/assets/AssetAsBuiltControllerFilteringIT.java
@@ -20,6 +20,7 @@
 package org.eclipse.tractusx.traceability.integration.assets;
 
 import io.restassured.http.ContentType;
+import org.eclipse.tractusx.traceability.assets.infrastructure.asbuilt.repository.JpaAssetAsBuiltRepository;
 import org.eclipse.tractusx.traceability.integration.IntegrationTestSpecification;
 import org.eclipse.tractusx.traceability.integration.common.support.AssetsSupport;
 import org.jose4j.lang.JoseException;
@@ -35,6 +36,8 @@ class AssetAsBuiltControllerFilteringIT extends IntegrationTestSpecification {
     @Autowired
     AssetsSupport assetsSupport;
 
+    @Autowired
+    JpaAssetAsBuiltRepository repo;
     @Test
     void givenNoFilter_whenCallFilteredEndpoint_thenReturnExpectedResult() throws JoseException {
         // given
@@ -51,6 +54,63 @@ class AssetAsBuiltControllerFilteringIT extends IntegrationTestSpecification {
                 .log().all()
                 .statusCode(200)
                 .body("totalItems", equalTo(13));
+    }
+
+    @Test
+    void givenInInvestigationFalseFilter_whenCallFilteredEndpoint_thenReturnExpectedResult() throws JoseException {
+        // given
+        assetsSupport.defaultAssetsStored();
+        final String filter = "?filter=activeAlert,EQUAL,false";
+
+        // then
+        given()
+                .header(oAuth2Support.jwtAuthorization(ADMIN))
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get("/api/assets/as-built/filtered" + filter)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("totalItems", equalTo(13));
+    }
+
+    @Test
+    void givenInInvestigationTrueFilter_whenCallFilteredEndpoint_thenReturnExpectedResult() throws JoseException {
+        // given
+        assetsSupport.defaultAssetsStored();
+        final String filter = "?filter=activeAlert,EQUAL,true";
+
+        // then
+        given()
+                .header(oAuth2Support.jwtAuthorization(ADMIN))
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get("/api/assets/as-built/filtered" + filter)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("totalItems", equalTo(0));
+    }
+
+    @Test
+    void givenIdAndIdShortFilter_whenCallFilteredEndpoint_thenReturnExpectedResult() throws JoseException {
+        // given
+        assetsSupport.defaultAssetsStored();
+        final String filter = "?filter=id,STARTS_WITH,urn:uuid:1&filter=idShort,STARTS_WITH,engineering";
+
+        // then
+        given()
+                .header(oAuth2Support.jwtAuthorization(ADMIN))
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get("/api/assets/as-built/filtered" + filter)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("totalItems", equalTo(2));
     }
 
     @Test
