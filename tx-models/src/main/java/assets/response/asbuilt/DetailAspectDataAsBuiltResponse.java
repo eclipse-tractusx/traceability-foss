10df93bd5884c70002624b0cd89c50a023269137
ds-lcapellino <lucas.capellino@doubleslash.de>
2023-09-12 17:27:47 +0200

    feature: TRACEFOSS-1731 update swagger to show possible interface implementations

--- tx-models/src/main/java/assets/response/asbuilt/DetailAspectDataAsBuiltResponse.java
+++ tx-models/src/main/java/assets/response/asbuilt/DetailAspectDataAsBuiltResponse.java
@@ -1,15 +1,27 @@
 package assets.response.asbuilt;
 
 import assets.response.base.DetailAspectDataResponse;
+import io.swagger.annotations.ApiModelProperty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class DetailAspectDataAsBuiltResponse implements DetailAspectDataResponse {
+    @ApiModelProperty(example = "95657762-59")
+    @Size(max = 255)
     private String partId;
+    @ApiModelProperty(example = "01697F7-65")
+    @Size(max = 255)
     private String customerPartId;
+    @ApiModelProperty(example = "Door front-left")
+    @Size(max = 255)
     private String nameAtCustomer;
+    @ApiModelProperty(example = "DEU")
+    @Size(max = 255)
     private String manufacturingCountry;
+    @ApiModelProperty(example = "2022-02-04T13:48:54Z")
+    @Size(max = 255)
     private String manufacturingDate;
 }
