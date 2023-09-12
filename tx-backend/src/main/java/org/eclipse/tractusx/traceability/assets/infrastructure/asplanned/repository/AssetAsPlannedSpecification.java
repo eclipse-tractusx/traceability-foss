e80617eed2b3afae8b844bb8c1fe937a8ddd7f58
ds-ext-sceronik <sebastian.ceronik@partner.doubleslash.de>
2023-09-12 13:35:50 +0200

    feature: TRACEFOSS-873 tests added

---
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/infrastructure/asplanned/repository/AssetAsPlannedSpecification.java
@@ -0,0 +1,80 @@
+/********************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+package org.eclipse.tractusx.traceability.assets.infrastructure.asplanned.repository;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.AllArgsConstructor;
+import org.eclipse.tractusx.traceability.assets.infrastructure.asplanned.model.AssetAsPlannedEntity;
+import org.eclipse.tractusx.traceability.common.model.SearchCriteria;
+import org.eclipse.tractusx.traceability.common.model.SearchOperation;
+import org.glassfish.jersey.internal.guava.Lists;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@AllArgsConstructor
+public class AssetAsPlannedSpecification implements Specification<AssetAsPlannedEntity> {
+
+    private SearchCriteria criteria;
+
+
+    @Override
+    public Predicate toPredicate(Root<AssetAsPlannedEntity> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        if (criteria.getOperation().equals(SearchOperation.EQUAL)) {
+            return builder.equal(
+                    root.<String>get(criteria.getKey()).as(String.class),
+                    (String) criteria.getValue());
+        }
+        if (criteria.getOperation().equals(SearchOperation.STARTS_WITH)) {
+            return builder.like(
+                    root.<String>get(criteria.getKey()),
+                    criteria.getValue() + "%");
+        }
+        if (criteria.getOperation().equals(SearchOperation.AT_LOCAL_DATE)) {
+            final LocalDate localDate = LocalDate.parse((String) criteria.getValue());
+            Predicate startingFrom = builder.greaterThanOrEqualTo(root.<LocalDateTime>get(criteria.getKey()),
+                    LocalDateTime.of(localDate, LocalTime.MIN));
+            Predicate endingAt = builder.lessThanOrEqualTo(root.<LocalDateTime>get(criteria.getKey()),
+                    LocalDateTime.of(localDate, LocalTime.MAX));
+            return builder.and(startingFrom, endingAt);
+        }
+        return null;
+    }
+
+    public static Specification<AssetAsPlannedEntity> toSpecification(final List<AssetAsPlannedSpecification> allSpecifications) {
+        var specifications = Lists.newArrayList(allSpecifications);
+        if (specifications.isEmpty()) {
+            return null;
+        }
+
+        Specification<AssetAsPlannedEntity> result = specifications.get(0);
+
+        for (int i = 1; i < specifications.size(); i++) {
+            result = Specification.where(result).and(specifications.get(i));
+        }
+        return result;
+    }
+}
