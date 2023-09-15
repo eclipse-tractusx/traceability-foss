680fbe5c1c29b5274088ba9c4fec46680008f4d7
ds-ext-sceronik <sebastian.ceronik@partner.doubleslash.de>
2023-09-15 11:04:06 +0200

    chore: TRACEFOSS-2537 fix test

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/qualitynotification/infrastructure/investigation/model/InvestigationEntity.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/qualitynotification/infrastructure/investigation/model/InvestigationEntity.java
@@ -74,28 +74,30 @@ public class InvestigationEntity extends NotificationBaseEntity {
     private List<InvestigationNotificationEntity> notifications;
 
 
-    public static QualityNotification toDomain(InvestigationEntity investigationNotificationEntity) {
-        List<QualityNotificationMessage> notifications = investigationNotificationEntity.getNotifications().stream()
+    public static QualityNotification toDomain(InvestigationEntity investigationEntity) {
+        List<QualityNotificationMessage> notifications = investigationEntity.getNotifications() == null
+                ? null
+                        : investigationEntity.getNotifications().stream()
                 .map(InvestigationNotificationEntity::toDomain)
                 .toList();
 
-        List<String> assetIds = investigationNotificationEntity.getAssets().stream()
+        List<String> assetIds = investigationEntity.getAssets().stream()
                 .map(AssetAsBuiltEntity::getId)
                 .toList();
 
         return QualityNotification.builder()
-                .notificationId(new QualityNotificationId(investigationNotificationEntity.getId()))
-                .bpn(BPN.of(investigationNotificationEntity.getBpn()))
-                .notificationStatus(QualityNotificationStatus.fromStringValue(investigationNotificationEntity.getStatus().name()))
-                .notificationSide(QualityNotificationSide.valueOf(investigationNotificationEntity.getSide().name()))
-                .closeReason(investigationNotificationEntity.getCloseReason())
-                .acceptReason(investigationNotificationEntity.getAcceptReason())
-                .declineReason(investigationNotificationEntity.getDeclineReason())
-                .createdAt(investigationNotificationEntity.getCreatedDate())
-                .description(investigationNotificationEntity.getDescription())
+                .notificationId(new QualityNotificationId(investigationEntity.getId()))
+                .bpn(BPN.of(investigationEntity.getBpn()))
+                .notificationStatus(QualityNotificationStatus.fromStringValue(investigationEntity.getStatus().name()))
+                .notificationSide(QualityNotificationSide.valueOf(investigationEntity.getSide().name()))
+                .closeReason(investigationEntity.getCloseReason())
+                .acceptReason(investigationEntity.getAcceptReason())
+                .declineReason(investigationEntity.getDeclineReason())
+                .createdAt(investigationEntity.getCreatedDate())
+                .description(investigationEntity.getDescription())
                 .assetIds(assetIds)
                 .notifications(notifications)
-                .errorMessage(investigationNotificationEntity.getErrorMessage())
+                .errorMessage(investigationEntity.getErrorMessage())
                 .build();
     }
 
