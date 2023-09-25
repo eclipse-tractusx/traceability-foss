dba2db7acf7920b90d367c4be9fdb589beddaf20
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-09-25 08:06:21 +0200

    chore: TRACEFOSS-2508 add unit test

--- frontend/src/app/modules/shared/components/bom-lifecycle-activator/bom-lifecycle-activator.component.spec.ts
+++ frontend/src/app/modules/shared/components/bom-lifecycle-activator/bom-lifecycle-activator.component.spec.ts
@@ -1,41 +1,89 @@
-/********************************************************************************
- * Copyright (c) 2023 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-
-import {SharedModule} from '@shared/shared.module';
 import {BomLifecycleActivatorComponent} from './bom-lifecycle-activator.component';
-import {renderComponent} from "@tests/test-render.utils";
-import {screen} from "@testing-library/angular";
-
+import {SharedModule} from '@shared/shared.module';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {I18NEXT_SERVICE, I18NextModule, ITranslationService} from "angular-i18next";
+import {KeycloakService} from "keycloak-angular";
+import {MockedKeycloakService} from "@core/auth/mocked-keycloak.service";
+import {APP_INITIALIZER} from "@angular/core";
 
 describe('BomLifecycleActivatorComponent', () => {
-    const renderBomLifecycleActivator = () => {
-        return renderComponent(`<app-bom-lifecycle-activator></app-bom-lifecycle-activator>`, {
-            declarations: [BomLifecycleActivatorComponent],
-            imports: [SharedModule]
-        });
-    };
 
-    it('should render the buttons', async () => {
-        await renderBomLifecycleActivator();
-        const asBuiltButton = screen.getByTestId("as-built-button");
-        const asPlannedButton = screen.getByTestId("as-planned-button");
-        expect(asBuiltButton).toBeInTheDocument();
-        expect(asPlannedButton).toBeInTheDocument();
+
+    let component: BomLifecycleActivatorComponent;
+    let fixture: ComponentFixture<BomLifecycleActivatorComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [BomLifecycleActivatorComponent],
+            imports: [I18NextModule.forRoot(), SharedModule],
+            providers: [
+                {
+                    provide: KeycloakService,
+                    useClass: MockedKeycloakService,
+                },
+                {
+                    provide: APP_INITIALIZER,
+                    useFactory: (i18next: ITranslationService) => {
+                        return () =>
+                            i18next.init({
+                                lng: 'en',
+                                supportedLngs: ['en', 'de'],
+                                resources: {},
+                            });
+                    },
+                    deps: [I18NEXT_SERVICE],
+                    multi: true,
+                }]
+        });
+        fixture = TestBed.createComponent(BomLifecycleActivatorComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should initialize bomLifecycleConfig correctly', () => {
+        expect(component.bomLifecycleConfig.asBuiltActive).toBe(true);
+        expect(component.bomLifecycleConfig.asPlannedActive).toBe(true);
+    });
+
+    it('should toggle asPlannedActive when toggleAsPlanned is called', () => {
+        component.toggleAsPlanned();
+        expect(component.bomLifecycleConfig.asPlannedActive).toBe(false);
+
+        component.toggleAsPlanned();
+        expect(component.bomLifecycleConfig.asPlannedActive).toBe(true);
+    });
+
+    it('should toggle asBuiltActive when toggleAsBuilt is called', () => {
+        component.toggleAsBuilt();
+        expect(component.bomLifecycleConfig.asBuiltActive).toBe(false);
+
+        component.toggleAsBuilt();
+        expect(component.bomLifecycleConfig.asBuiltActive).toBe(true);
+    });
+
+
+    it('should emit the correct size when only asPlannedActive is active', () => {
+        let emittedSize: any;
+        component.buttonClickEvent.subscribe((size) => {
+            emittedSize = size;
+        });
+
+        component.toggleAsBuilt();
+
+        expect(emittedSize).toEqual({asBuiltSize: 0, asPlannedSize: 100});
+    });
+
+    it('should emit the correct size when only asBuiltActive is active', () => {
+        let emittedSize: any;
+        component.buttonClickEvent.subscribe((size) => {
+            emittedSize = size;
+        });
+
+        component.toggleAsPlanned();
+
+        expect(emittedSize).toEqual({asBuiltSize: 100, asPlannedSize: 0});
     });
 });
