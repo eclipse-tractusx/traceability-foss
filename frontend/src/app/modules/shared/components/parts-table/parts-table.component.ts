0b1372f5abdc7fc24f5c727d788ae78263a74613
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-09-27 21:51:46 +0200

    chore: TRACEFOSS-1424 refactored

--- frontend/src/app/modules/shared/components/parts-table/parts-table.component.ts
+++ frontend/src/app/modules/shared/components/parts-table/parts-table.component.ts
@@ -19,262 +19,270 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-import { SelectionModel } from '@angular/cdk/collections';
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Pagination } from '@core/model/pagination.model';
-import { RoleService } from '@core/user/role.service';
-import { MenuActionConfig, TableConfig, TableEventConfig, TableHeaderSort } from '@shared/components/table/table.model';
-import { FlattenObjectPipe } from '@shared/pipes/flatten-object.pipe';
+import {SelectionModel} from '@angular/cdk/collections';
+import {Component, ElementRef, EventEmitter, Input, Output, ViewChild, ViewEncapsulation} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {MatPaginator, PageEvent} from '@angular/material/paginator';
+import {MatSort, Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {Pagination} from '@core/model/pagination.model';
+import {RoleService} from '@core/user/role.service';
+import {SemanticDataModel} from '@page/parts/model/parts.model';
 import {
-  MultiSelectAutocompleteComponent
-} from "@shared/components/multi-select-autocomplete/multi-select-autocomplete.component";
-import {FormControl, FormGroup} from "@angular/forms";
+    MultiSelectAutocompleteComponent
+} from '@shared/components/multi-select-autocomplete/multi-select-autocomplete.component';
+import {MenuActionConfig, TableConfig, TableEventConfig, TableHeaderSort} from '@shared/components/table/table.model';
+import {FlattenObjectPipe} from '@shared/pipes/flatten-object.pipe';
 
 @Component({
-  selector: 'app-parts-table',
-  templateUrl: './parts-table.component.html',
-  styleUrls: [ 'parts-table.component.scss' ],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-parts-table',
+    templateUrl: './parts-table.component.html',
+    styleUrls: ['parts-table.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class PartsTableComponent {
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild('tableElement', { read: ElementRef }) tableElementRef: ElementRef<HTMLElement>;
+    @ViewChild(MatSort) sort: MatSort;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild('tableElement', {read: ElementRef}) tableElementRef: ElementRef<HTMLElement>;
 
-  @Input()
-  set tableConfig(tableConfig: TableConfig) {
-    if (!tableConfig) {
-      return;
+    @Input()
+    set tableConfig(tableConfig: TableConfig) {
+        if (!tableConfig) {
+            return;
+        }
+
+        const {menuActionsConfig: menuActions, displayedColumns: dc, columnRoles, hasPagination = true} = tableConfig;
+        const displayedColumns = dc.filter(column => this.roleService.hasAccess(columnRoles?.[column] ?? 'user'));
+
+        const viewDetailsMenuAction: MenuActionConfig<unknown> = {
+            label: 'actions.viewDetails',
+            icon: 'remove_red_eye',
+            action: (data: Record<string, unknown>) => this.selected.emit(data),
+        };
+
+        const menuActionsConfig = menuActions ? [viewDetailsMenuAction, ...menuActions] : null;
+        this._tableConfig = {...tableConfig, displayedColumns, hasPagination, menuActionsConfig};
     }
 
-    const { menuActionsConfig: menuActions, displayedColumns: dc, columnRoles, hasPagination = true } = tableConfig;
-    const displayedColumns = dc.filter(column => this.roleService.hasAccess(columnRoles?.[column] ?? 'user'));
-
-    const viewDetailsMenuAction: MenuActionConfig<unknown> = {
-      label: 'actions.viewDetails',
-      icon: 'remove_red_eye',
-      action: (data: Record<string, unknown>) => this.selected.emit(data),
-    };
-
-    const menuActionsConfig = menuActions ? [ viewDetailsMenuAction, ...menuActions ] : null;
-    this._tableConfig = { ...tableConfig, displayedColumns, hasPagination, menuActionsConfig };
-  }
-
-  applyFilter(): void {
-    let data = this.dataSource.data.slice(); // Copy the data array
-    for (const key in this.filterValues) {
-      if (this.filterValues.hasOwnProperty(key)) {
-        const filterValue = this.filterValues[key].toLowerCase();
-        data = data.filter((item) => item[key].toLowerCase().includes(filterValue));
-      }
-    }
-    this.dataSource.data = data; // Update the table with filtered data
-  }
-
-  get tableConfig(): TableConfig {
-    return this._tableConfig;
-  }
-
-  @Input() labelId: string;
-  @Input() noShadow = false;
-  @Input() showHover = true;
-
-  @Input() selectedPartsInfoLabel: string;
-  @Input() selectedPartsActionLabel: string;
-
-  @Input() tableHeader: string;
-  @Input() multiSortList: TableHeaderSort[];
-
-
-  public readonly filterConfig: string[] = [
-    'Filter',
-    'b',
-    'c',
-    'd', // nameAtManufacturer
-    'e',
-    'f', // Part number / Batch Number / JIS Number
-    'g',
-    'ga', // --> semanticModel.customerPartId
-    'gb',
-    //'nameAtManufacturer', --> already in name
-    'bd', // --> semanticModel.nameAtCustomer
-    'de',
-    'ff',
-    'aa',
-    'gg',
-  ];
-
-  @Input() set paginationData({ page, pageSize, totalItems, content }: Pagination<unknown>) {
-    this.totalItems = totalItems;
-    this.pageSize = pageSize;
-    this.dataSource.data = content;
-    this.isDataLoading = false;
-    this.pageIndex = page;
-  }
-
-  @Input() set PartsPaginationData({ page, pageSize, totalItems, content }: Pagination<unknown>) {
-    let flatter = new FlattenObjectPipe();
-    // modify the content of the partlist so that there are no subobjects
-    let newContent = content.map(part => flatter.transform(part));
-    this.totalItems = totalItems;
-    this.pageSize = pageSize;
-    this.dataSource.data = newContent;
-    this.isDataLoading = false;
-    this.pageIndex = page;
-  }
-
-  @Input() set data(content: unknown[]) {
-
-    this.dataSource.data = content;
-    this.isDataLoading = false;
-  }
-
-  @Input() set deselectTrigger(deselectItem: unknown[]) {
-    if (!deselectItem) {
-      return;
+    applyFilter(): void {
+        let data = this.dataSource.data.slice(); // Copy the data array
+        for (const key in this.filterValues) {
+            if (this.filterValues.hasOwnProperty(key)) {
+                const filterValue = this.filterValues[key].toLowerCase();
+                data = data.filter((item) => item[key].toLowerCase().includes(filterValue));
+            }
+        }
+        this.dataSource.data = data; // Update the table with filtered data
     }
 
-    this.removeSelectedValues(deselectItem);
-    this.emitMultiSelect();
-  }
-
-  @Input() set addTrigger(newItem: unknown) {
-    if (!newItem) {
-      return;
+    get tableConfig(): TableConfig {
+        return this._tableConfig;
     }
 
-    this.selection.select(newItem);
-    this.emitMultiSelect();
-  }
+    @Input() labelId: string;
+    @Input() noShadow = false;
+    @Input() showHover = true;
 
-  @Output() selected = new EventEmitter<Record<string, unknown>>();
-  @Output() configChanged = new EventEmitter<TableEventConfig>();
-  @Output() multiSelect = new EventEmitter<any[]>();
-  @Output() clickSelectAction = new EventEmitter<void>();
+    @Input() selectedPartsInfoLabel: string;
+    @Input() selectedPartsActionLabel: string;
 
-  public readonly dataSource = new MatTableDataSource<unknown>();
-  public readonly selection = new SelectionModel<unknown>(true, []);
-  public filterValues = {};
-  public totalItems: number;
-  public pageIndex: number;
-  public isDataLoading: boolean;
-  public selectedRow: Record<string, unknown>;
-  public isMenuOpen: boolean;
-  public displayedFilter: boolean;
+    @Input() tableHeader: string;
+    @Input() multiSortList: TableHeaderSort[];
 
-  private pageSize: number;
-  private sorting: TableHeaderSort;
 
-  private _tableConfig: TableConfig;
+    public readonly filterConfig: string[] = [
+        'Filter',
+        'b',
+        'c',
+        'd', // nameAtManufacturer
+        'e',
+        'f', // Part number / Batch Number / JIS Number
+        'g',
+        'ga', // --> semanticModel.customerPartId
+        'gb',
+        //'nameAtManufacturer', --> already in name
+        'bd', // --> semanticModel.nameAtCustomer
+        'de',
+        'ff',
+        'aa',
+        'gg',
+    ];
 
-  constructor(private readonly roleService: RoleService) {
-  }
-
-  @ViewChild(MultiSelectAutocompleteComponent) multiSelection: MultiSelectAutocompleteComponent;
-
-  options = [
-    {
-      display: 'One',
-      value: '1'
-    }, {
-      display: 'Two',
-      value: '2'
-    }, {
-      display: 'Three',
-      value: '3'
-    }, {
-      display: 'Four',
-      value: '4'
-    }, {
-      display: 'Five',
-      value: '5'
-    }, {
-      display: 'Six',
-      value: '6'
+    @Input() set paginationData({page, pageSize, totalItems, content}: Pagination<unknown>) {
+        this.totalItems = totalItems;
+        this.pageSize = pageSize;
+        this.dataSource.data = content;
+        this.isDataLoading = false;
+        this.pageIndex = page;
     }
-  ];
-  profileForm = new FormGroup({
-    selected: new FormControl(['1', '2', '3'])
-  });
 
-
-  onToggleDropdown() {
-    this.multiSelection.toggleDropdown();
-  }
-  public areAllRowsSelected(): boolean {
-    return this.dataSource.data.every(data => this.isSelected(data));
-  }
-
-  public clearAllRows(): void {
-    this.selection.clear();
-    this.emitMultiSelect();
-  }
-
-  public clearCurrentRows(): void {
-    this.removeSelectedValues(this.dataSource.data);
-    this.emitMultiSelect();
-  }
-
-  public toggleAllRows(): void {
-    this.areAllRowsSelected()
-      ? this.removeSelectedValues(this.dataSource.data)
-      : this.addSelectedValues(this.dataSource.data);
-
-    this.emitMultiSelect();
-  }
-
-  public onPaginationChange({ pageIndex, pageSize }: PageEvent): void {
-    this.pageIndex = pageIndex;
-    this.isDataLoading = true;
-    this.configChanged.emit({ page: pageIndex, pageSize: pageSize, sorting: this.sorting });
-  }
-
-  public updateSortingOfData({ active, direction }: Sort): void {
-    this.selection.clear();
-    this.emitMultiSelect();
-    this.sorting = !direction ? null : ([ active, direction ] as TableHeaderSort);
-    this.isDataLoading = true;
-    this.configChanged.emit({ page: 0, pageSize: this.pageSize, sorting: this.sorting });
-  }
-
-  public toggleSelection(row: unknown): void {
-    this.isSelected(row) ? this.removeSelectedValues([ row ]) : this.addSelectedValues([ row ]);
-    this.emitMultiSelect();
-  }
-
-  public selectElement(row: Record<string, unknown>) {
-    this.selectedRow = this.selectedRow === row ? null : row;
-
-    if (!this.tableConfig.menuActionsConfig) {
-      this.selected.emit(row);
+    @Input() set PartsPaginationData({page, pageSize, totalItems, content}: Pagination<unknown>) {
+        let flatter = new FlattenObjectPipe();
+        // modify the content of the partlist so that there are no subobjects
+        let newContent = content.map(part => flatter.transform(part));
+        this.totalItems = totalItems;
+        this.pageSize = pageSize;
+        this.dataSource.data = newContent;
+        this.isDataLoading = false;
+        this.pageIndex = page;
     }
-  }
 
-  private emitMultiSelect(): void {
-    this.multiSelect.emit(this.selection.selected);
-  }
+    @Input() set data(content: unknown[]) {
 
-  public toggleFilter(): void {
-    this.displayedFilter = !this.displayedFilter;
-    console.log(this.displayedFilter);
-  }
+        this.dataSource.data = content;
+        this.isDataLoading = false;
+    }
 
-  public isSelected(row: unknown): boolean {
-    return !!this.selection.selected.find(data => JSON.stringify(data) === JSON.stringify(row));
-  }
+    @Input() set deselectTrigger(deselectItem: unknown[]) {
+        if (!deselectItem) {
+            return;
+        }
 
-  private addSelectedValues(newData: unknown[]): void {
-    const newValues = newData.filter(data => !this.isSelected(data));
-    this.selection.select(...newValues);
-  }
+        this.removeSelectedValues(deselectItem);
+        this.emitMultiSelect();
+    }
 
-  private removeSelectedValues(itemsToRemove: unknown[]): void {
-    const shouldDelete = (row: unknown) => !!itemsToRemove.find(data => JSON.stringify(data) === JSON.stringify(row));
-    const rowsToDelete = this.selection.selected.filter(data => shouldDelete(data));
+    @Input() set addTrigger(newItem: unknown) {
+        if (!newItem) {
+            return;
+        }
 
-    this.selection.deselect(...rowsToDelete);
-  }
+        this.selection.select(newItem);
+        this.emitMultiSelect();
+    }
+
+    @Output() selected = new EventEmitter<Record<string, unknown>>();
+    @Output() configChanged = new EventEmitter<TableEventConfig>();
+    @Output() multiSelect = new EventEmitter<any[]>();
+    @Output() clickSelectAction = new EventEmitter<void>();
+
+    public readonly dataSource = new MatTableDataSource<unknown>();
+    public readonly selection = new SelectionModel<unknown>(true, []);
+    public filterValues = {};
+    public totalItems: number;
+    public pageIndex: number;
+    public isDataLoading: boolean;
+    public selectedRow: Record<string, unknown>;
+    public isMenuOpen: boolean;
+    public displayedFilter: boolean;
+
+    private pageSize: number;
+    private sorting: TableHeaderSort;
+
+    private _tableConfig: TableConfig;
+
+    filterFormGroup = new FormGroup({
+        id: new FormControl([""]),
+        semanticDataModel: new FormControl([SemanticDataModel.BATCH, SemanticDataModel.JUSTINSEQUENCEPART, SemanticDataModel.SERIALPART, SemanticDataModel.PARTASPLANNED, SemanticDataModel.UNKNOWN]),
+    });
+
+    constructor(private readonly roleService: RoleService) {
+        this.filterFormGroup.get('semanticDataModel').valueChanges.subscribe((selectedValues) => {
+            // This function will be called whenever the 'selected' control's value changes
+            console.log('Selected values changed:', selectedValues);
+
+            // You can perform any actions you want here based on the new values.
+        });
+    }
+
+    @ViewChild(MultiSelectAutocompleteComponent) multiSelection: MultiSelectAutocompleteComponent;
+
+    semanticDataModelOptions = [
+        {
+            display: 'Batch',
+            value: SemanticDataModel.BATCH,
+        }, {
+            display: 'JustInSequence',
+            value: SemanticDataModel.JUSTINSEQUENCEPART,
+        }, {
+            display: 'SerialPart',
+            value: SemanticDataModel.SERIALPART,
+        }, {
+            display: 'Unknown',
+            value: SemanticDataModel.UNKNOWN,
+        }, {
+            display: 'PartAsPlanned',
+            value: SemanticDataModel.PARTASPLANNED,
+        },
+    ];
+
+
+    onToggleDropdown() {
+        this.multiSelection.toggleDropdown();
+    }
+
+    public areAllRowsSelected(): boolean {
+        return this.dataSource.data.every(data => this.isSelected(data));
+    }
+
+    public clearAllRows(): void {
+        this.selection.clear();
+        this.emitMultiSelect();
+    }
+
+    public clearCurrentRows(): void {
+        this.removeSelectedValues(this.dataSource.data);
+        this.emitMultiSelect();
+    }
+
+    public toggleAllRows(): void {
+        this.areAllRowsSelected()
+            ? this.removeSelectedValues(this.dataSource.data)
+            : this.addSelectedValues(this.dataSource.data);
+
+        this.emitMultiSelect();
+    }
+
+    public onPaginationChange({pageIndex, pageSize}: PageEvent): void {
+        this.pageIndex = pageIndex;
+        this.isDataLoading = true;
+        this.configChanged.emit({page: pageIndex, pageSize: pageSize, sorting: this.sorting});
+    }
+
+    public updateSortingOfData({active, direction}: Sort): void {
+        this.selection.clear();
+        this.emitMultiSelect();
+        this.sorting = !direction ? null : ([active, direction] as TableHeaderSort);
+        this.isDataLoading = true;
+        this.configChanged.emit({page: 0, pageSize: this.pageSize, sorting: this.sorting});
+    }
+
+    public toggleSelection(row: unknown): void {
+        this.isSelected(row) ? this.removeSelectedValues([row]) : this.addSelectedValues([row]);
+        this.emitMultiSelect();
+    }
+
+    public selectElement(row: Record<string, unknown>) {
+        this.selectedRow = this.selectedRow === row ? null : row;
+
+        if (!this.tableConfig.menuActionsConfig) {
+            this.selected.emit(row);
+        }
+    }
+
+
+    private emitMultiSelect(): void {
+        this.multiSelect.emit(this.selection.selected);
+    }
+
+    public toggleFilter(): void {
+        this.displayedFilter = !this.displayedFilter;
+        console.log(this.displayedFilter);
+    }
+
+    public isSelected(row: unknown): boolean {
+        return !!this.selection.selected.find(data => JSON.stringify(data) === JSON.stringify(row));
+    }
+
+    private addSelectedValues(newData: unknown[]): void {
+        const newValues = newData.filter(data => !this.isSelected(data));
+        this.selection.select(...newValues);
+    }
+
+    private removeSelectedValues(itemsToRemove: unknown[]): void {
+        const shouldDelete = (row: unknown) => !!itemsToRemove.find(data => JSON.stringify(data) === JSON.stringify(row));
+        const rowsToDelete = this.selection.selected.filter(data => shouldDelete(data));
+
+        this.selection.deselect(...rowsToDelete);
+    }
 }
