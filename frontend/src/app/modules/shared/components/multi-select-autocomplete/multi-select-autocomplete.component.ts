bbe32016ff84622351ca2bb3957a6697f03616a7
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-09-28 17:03:26 +0200

    chore: TRACEFOSS-1424 added config object for multiselect

--- frontend/src/app/modules/shared/components/multi-select-autocomplete/multi-select-autocomplete.component.ts
+++ frontend/src/app/modules/shared/components/multi-select-autocomplete/multi-select-autocomplete.component.ts
@@ -17,170 +17,193 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-import { Component, EventEmitter, Input, OnChanges, Output, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, Output, ViewChild} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-multiselect',
-  templateUrl: 'multi-select-autocomplete.component.html',
-  styleUrls: ['multi-select-autocomplete.component.scss']
+    selector: 'app-multiselect',
+    templateUrl: 'multi-select-autocomplete.component.html',
+    styleUrls: ['multi-select-autocomplete.component.scss']
 })
 
 export class MultiSelectAutocompleteComponent implements OnChanges {
 
-  @Input()
-  placeholder;
-  @Input()
-  options;
-  @Input()
-  disabled = false;
-  @Input()
-  display = 'display';
-  @Input()
-  value = 'value';
-  @Input()
-  formControl = new FormControl();
-  @Input()
-  errorMsg = 'Field is required';
-  @Input()
-  showErrorMsg = false;
-  @Input()
-  selectedOptions;
-  @Input()
-  multiple = true;
-  @Input()
-  textSearch = true;
+    @Input()
+    placeholder;
+    @Input()
+    options;
+    @Input()
+    disabled = false;
+    @Input()
+    display = 'display';
+    @Input()
+    value = 'value';
+    @Input()
+    formControl = new FormControl();
+    @Input()
+    errorMsg = 'Field is required';
+    @Input()
+    showErrorMsg = false;
+    @Input()
+    selectedOptions;
+    @Input()
+    multiple = true;
+    @Input()
+    textSearch = true;
 
-  // New Options
-  @Input()
-  labelCount = 1;
-  @Input()
-  appearance = 'standard';
+    @Input()
+    name = '';
 
-  @Output()
-  selectionChange: EventEmitter<any> = new EventEmitter();
+    // New Options
+    @Input()
+    labelCount = 1;
+    @Input()
+    appearance = 'standard';
 
-  @ViewChild('selectElem') selectElem;
+    @ViewChild('searchInput') searchInput: ElementRef;
 
-  filteredOptions: Array<any> = [];
-  selectedValue: Array<any> = [];
-  selectAllChecked = false;
-  displayString = '';
 
-  constructor() {
-  }
+    theSearchElement: string = '';
 
-  ngOnChanges() {
-    this.filteredOptions = this.options;
-    if (this.selectedOptions) {
-      this.selectedValue = this.selectedOptions;
-    } else if (this.formControl.value) {
-      this.selectedValue = this.formControl.value;
+    @Output()
+    selectionChange: EventEmitter<any> = new EventEmitter();
+
+    @ViewChild('selectElem') selectElem;
+
+    filteredOptions: Array<any> = [];
+    selectedValue: Array<any> = [];
+    selectAllChecked = false;
+    displayString = '';
+
+    constructor() {
     }
 
-  }
+    ngOnChanges() {
+        console.log("trigger on change");
 
-  toggleDropdown() {
-    this.selectElem.toggle();
-  }
-
-  toggleSelectAll = function(val) {
-    if (val.checked) {
-      this.filteredOptions.forEach(option => {
-        if (!this.selectedValue.includes(option[this.value])) {
-          this.selectedValue = this.selectedValue.concat([ option[this.value] ]);
+        this.filteredOptions = this.options;
+        if (!this.filteredOptions) {
+            console.log("options empty");
         }
-      });
-    } else {
-      const filteredValues = this.getFilteredOptionsValues();
-      this.selectedValue = this.selectedValue.filter(
-        item => !filteredValues.includes(item),
-      );
-    }
-    this.selectionChange.emit(this.selectedValue);
-  };
-
-  filterItem(value) {
-    if (this.textSearch){
-      console.log("this should be a text search");
-    } else {
-      console.log("this should be a static option search");
-      this.filteredOptions = this.options.filter(
-          item => item[this.display].toLowerCase().indexOf(value.toLowerCase()) > -1,
-      );
-      this.selectAllChecked = true;
-      this.filteredOptions.forEach(item => {
-        if (!this.selectedValue.includes(item[this.value])) {
-          this.selectAllChecked = false;
+        if (this.selectedOptions) {
+            this.selectedValue = this.selectedOptions;
+        } else if (this.formControl.value) {
+            this.selectedValue = this.formControl.value;
         }
-      });
+
     }
 
-  }
-
-  hideOption(option) {
-    return !(this.filteredOptions.indexOf(option) > -1);
-  }
-
-  // Returns plain strings array of filtered values
-  getFilteredOptionsValues() {
-    const filteredValues = [];
-    this.filteredOptions.forEach(option => {
-      filteredValues.push(option.value);
-    });
-    return filteredValues;
-  }
-
-  onDisplayString() {
-    this.displayString = '';
-    if (this.selectedValue && this.selectedValue.length) {
-      let displayOption = [];
-      if (this.multiple) {
-        // Multi select display
-        for (let i = 0; i < this.labelCount; i++) {
-
-          displayOption[i] = this.options.filter(
-            option => option.value === this.selectedValue[i],
-          )[0];
-        }
-        if (displayOption.length) {
-          for (let i = 0; i < displayOption.length; i++) {
-
-            this.displayString += displayOption[i][this.display] + ',';
-          }
-          this.displayString = this.displayString.slice(0, -1);
-          if (this.selectedValue.length === this.options.length) {
-            this.displayString = 'All';
-          } else if (this.selectedValue.length > 1) {
-            this.displayString += ` (+${this.selectedValue.length - this.labelCount} others)`;
-          }
-        }
-      } else {
-        // Single select display
-        displayOption = this.options.filter(
-          option => option[this.value] === this.selectedValue,
-        );
-        if (displayOption.length) {
-          this.displayString = displayOption[0][this.display];
-        }
-      }
+    toggleDropdown() {
+        this.selectElem.toggle();
     }
-    return this.displayString;
-  }
 
-  onSelectionChange(val) {
-    const filteredValues = this.getFilteredOptionsValues();
-    let count = 0;
-    if (this.multiple) {
-      this.selectedValue.filter(item => {
-        if (filteredValues.includes(item)) {
-          count++;
+    toggleSelectAll = function (val) {
+        if (val.checked) {
+            this.filteredOptions.forEach(option => {
+                if (!this.selectedValue.includes(option[this.value])) {
+                    this.selectedValue = this.selectedValue.concat([option[this.value]]);
+                }
+            });
+        } else {
+            const filteredValues = this.getFilteredOptionsValues();
+            this.selectedValue = this.selectedValue.filter(
+                item => !filteredValues.includes(item),
+            );
         }
-      });
-      this.selectAllChecked = count === this.filteredOptions.length;
+        this.selectionChange.emit(this.selectedValue);
+    };
+
+    filterItem(value) {
+        if (this.textSearch) {
+
+        } else {
+
+            this.filteredOptions = this.options.filter(
+                item => item[this.display].toLowerCase().indexOf(value.toLowerCase()) > -1,
+            );
+            this.selectAllChecked = true;
+            this.filteredOptions.forEach(item => {
+                if (!this.selectedValue.includes(item[this.value])) {
+                    this.selectAllChecked = false;
+                }
+            });
+        }
+
     }
-    this.selectedValue = val.value;
-    this.selectionChange.emit(this.selectedValue);
-  }
+
+    hideOption(option) {
+        return !(this.filteredOptions.indexOf(option) > -1);
+    }
+
+    // Returns plain strings array of filtered values
+    getFilteredOptionsValues() {
+        const filteredValues = [];
+        this.filteredOptions.forEach(option => {
+            filteredValues.push(option.value);
+        });
+        return filteredValues;
+    }
+
+    onDisplayString() {
+        //console.log("Name: " + this.name + " isTextSearch " + this.textSearch);
+        if (this.textSearch) {
+
+            this.displayString = this.theSearchElement;
+            return this.displayString;
+        }
+
+        this.displayString = '';
+        if (this.selectedValue && this.selectedValue.length) {
+            let displayOption = [];
+            if (this.multiple) {
+                // Multi select display
+                for (let i = 0; i < this.labelCount; i++) {
+
+                    displayOption[i] = this.options.filter(
+                        option => option.value === this.selectedValue[i],
+                    )[0];
+                }
+                if (displayOption.length) {
+                    for (let i = 0; i < displayOption.length; i++) {
+
+                        this.displayString += displayOption[i][this.display] + ',';
+                    }
+                    this.displayString = this.displayString.slice(0, -1);
+                    if (this.selectedValue.length === this.options.length) {
+                        this.displayString = 'All';
+                    } else if (this.selectedValue.length > 1) {
+                        this.displayString += ` (+${this.selectedValue.length - this.labelCount} others)`;
+                    }
+                }
+            } else {
+                // Single select display
+                displayOption = this.options.filter(
+                    option => option[this.value] === this.selectedValue,
+                );
+                if (displayOption.length) {
+                    this.displayString = displayOption[0][this.display];
+                }
+            }
+        }
+
+        return this.displayString;
+    }
+
+    onSelectionChange(val, textSearch) {
+
+        console.log("selection change");
+        const filteredValues = this.getFilteredOptionsValues();
+        let count = 0;
+        if (this.multiple) {
+            this.selectedValue.filter(item => {
+                if (filteredValues.includes(item)) {
+                    count++;
+                }
+            });
+            this.selectAllChecked = count === this.filteredOptions.length;
+        }
+        this.selectedValue = val.value;
+        this.selectionChange.emit(this.selectedValue);
+    }
 
 }
