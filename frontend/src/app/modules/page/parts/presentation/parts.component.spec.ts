5fb47816c66fbfe78af06cccefab94d0cf728150
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-09-29 22:15:07 +0200

    chore: TRACEFOSS-1424 removed tests which should be tested in the table itself

--- frontend/src/app/modules/page/parts/presentation/parts.component.spec.ts
+++ frontend/src/app/modules/page/parts/presentation/parts.component.spec.ts
@@ -19,45 +19,81 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-import { By } from '@angular/platform-browser';
-import { LayoutModule } from '@layout/layout.module';
-import { SidenavComponent } from '@layout/sidenav/sidenav.component';
-import { SidenavService } from '@layout/sidenav/sidenav.service';
-import { OtherPartsModule } from '@page/other-parts/other-parts.module';
-import { PartsComponent } from '@page/parts/presentation/parts.component';
-import { SharedModule } from '@shared/shared.module';
-import { fireEvent, screen, waitFor } from '@testing-library/angular';
-import { renderComponent } from '@tests/test-render.utils';
-import { PartsModule } from '../parts.module';
-import {PartsTableComponent} from "@shared/components/parts-table/parts-table.component";
-import {
-  MultiSelectAutocompleteComponent
-} from "@shared/components/multi-select-autocomplete/multi-select-autocomplete.component";
+import {LayoutModule} from '@layout/layout.module';
+import {SidenavComponent} from '@layout/sidenav/sidenav.component';
+import {SidenavService} from '@layout/sidenav/sidenav.service';
+import {OtherPartsModule} from '@page/other-parts/other-parts.module';
+import {PartsComponent} from '@page/parts/presentation/parts.component';
+import {SharedModule} from '@shared/shared.module';
+import {screen, waitFor} from '@testing-library/angular';
+import {renderComponent} from '@tests/test-render.utils';
+import {PartsModule} from '../parts.module';
+import {AssetAsBuiltFilter} from "@page/parts/model/parts.model";
 
 describe('Parts', () => {
-  const renderParts = () => {
-    return renderComponent(`
-      <app-sidenav></app-sidenav>
-      <app-parts></app-parts>`, {
-      declarations: [SidenavComponent, PartsComponent],
-      imports: [PartsModule, SharedModule, LayoutModule, OtherPartsModule],
-      providers: [{ provide: SidenavService }],
-      roles: ['admin', 'wip'],
+
+    const renderParts = () => {
+        return renderComponent(PartsComponent, {
+            declarations: [SidenavComponent],
+            imports: [PartsModule, SharedModule, LayoutModule, OtherPartsModule],
+            providers: [{provide: SidenavService}],
+            roles: ['admin', 'wip'],
+        });
+    };
+
+    it('should render split areas', async () => {
+        await renderParts();
+        expect(await waitFor(() => screen.getByTestId('as-split-area-1-component--test-id'))).toBeInTheDocument();
+        expect(await waitFor(() => screen.getByTestId('as-split-area-2-component--test-id'))).toBeInTheDocument();
     });
-  };
 
-  it('should render split areas', async () => {
-    await renderParts();
-    expect(await waitFor(() => screen.getByTestId('as-split-area-1-component--test-id'))).toBeInTheDocument();
-    expect(await waitFor(() => screen.getByTestId('as-split-area-2-component--test-id'))).toBeInTheDocument();
-  });
+    it('should render parts with closed sidenav', async () => {
+        await renderParts();
+        const sideNavElement = await waitFor(() => screen.getByTestId('sidenav--test-id'));
+        expect(sideNavElement).toBeInTheDocument();
+        expect(sideNavElement).not.toHaveClass('sidenav--container__open');
+    });
 
-  it('should render parts with closed sidenav', async () => {
-    await renderParts();
+    it('should have correct sizes for split areas', async () => {
+        const {fixture} = await renderParts();
+        const {componentInstance} = fixture;
+        expect(componentInstance.bomLifecycleSize.asPlannedSize).toBe(50);
+        expect(componentInstance.bomLifecycleSize.asBuiltSize).toBe(50);
+    });
 
-    const sideNavElement = await waitFor(() => screen.getByTestId('sidenav--test-id'));
-    expect(sideNavElement).toBeInTheDocument();
-    expect(sideNavElement).not.toHaveClass('sidenav--container__open');
-  });
+
+    it('should call partsFacade.setPartsAsBuiltWithFilter when filter is set', async () => {
+
+        const {fixture} = await renderParts();
+        const {componentInstance} = fixture;
+        // Arrange
+        const assetAsBuiltFilter: AssetAsBuiltFilter = {
+            id: "123"
+        };
+        const partsFacade = (componentInstance as any)['partsFacade'];
+        const partsFacadeSpy = spyOn(partsFacade, 'setPartsAsBuiltWithFilter');
+
+
+        componentInstance.filterActivatedAsBuilt(assetAsBuiltFilter);
+
+
+        expect(partsFacadeSpy).toHaveBeenCalledWith(0, 50, [], assetAsBuiltFilter);
+    });
+
+    it('should call partsFacade.setPartsAsBuilt when filter is not set', async () => {
+
+        const {fixture} = await renderParts();
+        const {componentInstance} = fixture;
+
+        const assetAsBuiltFilter: AssetAsBuiltFilter = {};
+        const partsFacade = (componentInstance as any)['partsFacade'];
+        const partsFacadeSpy = spyOn(partsFacade, 'setPartsAsBuilt');
+
+        // Act
+        componentInstance.filterActivatedAsBuilt(assetAsBuiltFilter);
+
+        // Assert
+        expect(partsFacadeSpy).toHaveBeenCalledWith();
+    });
 
 });
