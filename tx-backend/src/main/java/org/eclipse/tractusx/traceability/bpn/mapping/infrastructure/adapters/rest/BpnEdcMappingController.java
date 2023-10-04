b940be1485be281732cd6fb3d613aaab1c541194
ashanmugavel <abilash.shanmugavel@doubleslash.com>
2023-10-04 09:08:42 +0200

    fixed: Response Code Missing (v3)

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/bpn/mapping/infrastructure/adapters/rest/BpnEdcMappingController.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/bpn/mapping/infrastructure/adapters/rest/BpnEdcMappingController.java
@@ -36,15 +36,7 @@ import org.eclipse.tractusx.traceability.common.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -266,6 +258,12 @@ public class BpnEdcMappingController {
             @ApiResponse(
                     responseCode = "404",
                     description = "Not found.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(
+                    responseCode = "415",
+                    description = "Unsupported media type",
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = ErrorResponse.class))),
