df2d1cc83b162db19ab5c8548d92fb1c9d6c3f32
ashanmugavel <abilash.shanmugavel@doubleslash.com>
2023-10-09 12:48:31 +0200

    fixed: Missing Override annotation

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/qualitynotification/infrastructure/investigation/model/InvestigationNotificationEntity.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/qualitynotification/infrastructure/investigation/model/InvestigationNotificationEntity.java
@@ -48,7 +48,7 @@ import java.util.List;
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "investigation_notification")
 public class InvestigationNotificationEntity extends QualityNotificationMessageBaseEntity {
