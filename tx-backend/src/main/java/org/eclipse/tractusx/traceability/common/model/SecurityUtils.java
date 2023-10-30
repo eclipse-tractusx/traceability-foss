21356e307dd4d98ca0ce0a7b01d2091937163abb
ashanmugavel <abilash.shanmugavel@doubleslash.com>
2023-10-30 16:02:53 +0100

    chore:[TRACEFOSS-XXX] fixed NullPointerException

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/model/SecurityUtils.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/model/SecurityUtils.java
@@ -29,7 +29,7 @@ import qualitynotification.base.request.StartQualityNotificationRequest;
 import qualitynotification.base.request.UpdateQualityNotificationRequest;
 
 import java.util.List;
-import java.util.Objects;
+
 
 public class SecurityUtils {
 
