8deee105e5bc7c4bb1116ef0c7eb63ebd96cafb1
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-10-31 16:54:31 +0100

    chore: TRACEFOSS-2692 rework tables

--- frontend/cypress/support/step_definitions/login.ts
+++ frontend/cypress/support/step_definitions/login.ts
@@ -1,15 +1,14 @@
-import { Given, Then } from '@badeball/cypress-cucumber-preprocessor';
-import { DashboardPage } from '../../integration/pages/DashboardPage';
+import { Given } from '@badeball/cypress-cucumber-preprocessor';
 
 
 Given(/^user logged in as "supervisor"$/, () => {
   // Simulate the login process here, including entering credentials and submitting the form
-  const supervisorLogin = Cypress.env("SUPERVISOR_LOGIN");
-  const supervisorPW = Cypress.env("SUPERVISOR_PW");
-  console.log(Cypress, "CYPRESS");
-  console.log(Cypress.env, "CYPRESS.env");
-  console.log(Cypress.env("CYPRESS_SUPERVISOR_LOGIN"), "Cypress.env(CYPRESS_SUPERVISOR_LOGIN");
-  console.log(Cypress.env("SUPERVISOR_LOGIN"), "Cypress.env(SUPERVISOR_LOGIN)");
+  const supervisorLogin = Cypress.env('SUPERVISOR_LOGIN');
+  const supervisorPW = Cypress.env('SUPERVISOR_PW');
+  console.log(Cypress, 'CYPRESS');
+  console.log(Cypress.env, 'CYPRESS.env');
+  console.log(Cypress.env('CYPRESS_SUPERVISOR_LOGIN'), 'Cypress.env(CYPRESS_SUPERVISOR_LOGIN');
+  console.log(Cypress.env('SUPERVISOR_LOGIN'), 'Cypress.env(SUPERVISOR_LOGIN)');
 
 
   cy.visit('https://centralidp.dev.demo.catena-x.net/auth/realms/CX-Central/protocol/openid-connect/auth?client_id=Cl17-CX-Part&redirect_uri=https%3A%2F%2Ftraceability-portal-e2e-a.dev.demo.catena-x.net%2Fdashboard&state=0aaee615-388e-400c-8b0c-81ac443a2cf3&response_mode=fragment&response_type=code&scope=openid&nonce=4104d5ab-b2bd-43a1-b6c2-7adf30543579&code_challenge=uXHR3gDRnSyjPEu8yWNdzm6Izsd7cKzEryfvRAtJTjU&code_challenge_method=S256');
@@ -23,4 +22,9 @@ Given(/^user logged in as "supervisor"$/, () => {
 
   cy.get('input[type="submit"]').click();
 
+});
+
+
+Given('user is directed to the {string}', function(string) {
+  cy.get('div.layout-content').should('exist');
 });
