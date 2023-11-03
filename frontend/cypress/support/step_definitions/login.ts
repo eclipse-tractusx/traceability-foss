00dd78c9ccf98b808cc1f7d3145bc40d706d92b8
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2023-11-03 11:15:47 +0100

    chore: TRACEFOSS-1040 added credentials as secrets to the cypress tests.

--- frontend/cypress/support/step_definitions/login.ts
+++ frontend/cypress/support/step_definitions/login.ts
@@ -3,8 +3,8 @@ import { Given } from '@badeball/cypress-cucumber-preprocessor';
 
 Given(/^user logged in as "supervisor"$/, () => {
   // Simulate the login process here, including entering credentials and submitting the form
-  const supervisorLogin = Cypress.env('TRACE_X_USER_LOGIN');
-  const supervisorPW = Cypress.env('TRACE_X_USER_PW');
+  const supervisorLogin = Cypress.env('SUPERVISOR_LOGIN');
+  const supervisorPW = Cypress.env('SUPERVISOR_PW');
 
 
   cy.visit('https://centralidp.dev.demo.catena-x.net/auth/realms/CX-Central/protocol/openid-connect/auth?client_id=Cl17-CX-Part&redirect_uri=https%3A%2F%2Ftraceability-portal-e2e-a.dev.demo.catena-x.net%2Fdashboard&state=0aaee615-388e-400c-8b0c-81ac443a2cf3&response_mode=fragment&response_type=code&scope=openid&nonce=4104d5ab-b2bd-43a1-b6c2-7adf30543579&code_challenge=uXHR3gDRnSyjPEu8yWNdzm6Izsd7cKzEryfvRAtJTjU&code_challenge_method=S256');
