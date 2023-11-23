7cf65dcdd53d09971e3e1a45e28469b3a3fa0f4c
ds-alexander-bulgakov <alexander.bulgakov@partner.doubleslash.de>
2023-11-23 07:54:30 +0100

    feat(testing):[TRACEFOSS-2838] tryouts and fix while testing

--- frontend/cypress/support/step_definitions/quality-investigations.ts
+++ frontend/cypress/support/step_definitions/quality-investigations.ts
@@ -28,9 +28,11 @@ let notificationDescription = null;
 
 Then(/^select "one" other part$/, () => {
 //since IDs of desired asset are not shown in FE the selection has to be done by other number
-  cy.wait(500);
-  cy.get('span').should('contain.text', 'NO-989134870198932317923938').parent('.row').as('part');
-  cy.get('@part').get('#mat-mds-checkbox-21').click();
+//   cy.wait(500);
+//   cy.get('span').should('contain.text', 'NO-989134870198932317923938').parent('.row').as('part');
+//   cy.get('@part').get('#mat-mds-checkbox-21').click();
+
+  cy.get('#mat-mdc-checkbox-38').click();
 });
 
 Then(/^start investigation creation with description {string}$/, (description) => {
