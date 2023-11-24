f523e1c473fd0474341b411ad6a0ee96a91287f2
ds-alexander-bulgakov <alexander.bulgakov@partner.doubleslash.de>
2023-11-24 10:26:50 +0100

    feat(testing):[TRACEFOSS-2838] tryouts and fix while testing

--- frontend/cypress/support/step_definitions/quality-investigations.ts
+++ frontend/cypress/support/step_definitions/quality-investigations.ts
@@ -33,7 +33,8 @@ Then("select {string} other part", (partAmount) => {
   cy.get('span').contains('As Planned').click(); // see comment above. This has to be done to avoid asPlanned selection
   // cy.get('#mat-mdc-checkbox-38').click(); //---TBD--- this is only a method to make it run, has to be changed to selected part as above!
 //   cy.get('span').contains('NO-989134870198932317923938').parentsUntil('.mat-mdc-row mdc-data-table__row cdk-row ng-star-inserted').first().get('[type="checkbox"]').click();
-  cy.get('span').contains('NO-989134870198932317923938').parentsUntil('.mat-mdc-row mdc-data-table__row cdk-row ng-star-inserted').children().get('[type="checkbox"]').last().click();
+  //cy.get('span').contains('NO-989134870198932317923938').parentsUntil('.mat-mdc-row mdc-data-table__row cdk-row ng-star-inserted').children().get('[type="checkbox"]').first().click();
+  cy.get('span').contains('NO-989134870198932317923938').prevUntil('.mat-mdc-row mdc-data-table__row cdk-row ng-star-inserted').get('[type="checkbox"]').first().click();
 });
 
 Then("start investigation creation with description {string}", function (description) {
