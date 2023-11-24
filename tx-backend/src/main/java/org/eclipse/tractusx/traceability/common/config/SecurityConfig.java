b1fec28a2e6ba37f1cfe2b4dc015fbe795770515
Jan Kreutzfeld <100288948+ds-jkreutzfeld@users.noreply.github.com>
2023-11-24 09:37:59 +0100

    chore(security):[DO-4230] Add missing security headers, replace calls to deprecated methods with new API design (#56)

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/config/SecurityConfig.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/config/SecurityConfig.java
@@ -26,13 +26,19 @@ import org.eclipse.tractusx.traceability.common.security.JwtAuthenticationTokenC
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.PermissionsPolicyHeaderWriter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -42,6 +48,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private static final long HSTS_MAX_AGE_DAYS = 365;
+
+    private static final String PERMISSION_POLICY = "microphone=(), geolocation=(), camera=()";
 
     private static final String[] WHITELIST_PATHS = {
             "/swagger-ui.html",
@@ -65,15 +74,27 @@ public class SecurityConfig {
 
 
     @Bean
-    SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity, @Value("${cors.origins}") final List<String> origins) throws Exception {
 
-        httpSecurity.httpBasic().disable();
-        httpSecurity.formLogin().disable();
-        httpSecurity.logout().disable();
-        httpSecurity.anonymous().disable();
-        httpSecurity.csrf().disable();
-        httpSecurity.cors();
+        httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(AbstractHttpConfigurer::disable);
+        httpSecurity.anonymous(AbstractHttpConfigurer::disable);
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
+        httpSecurity.cors(customizer -> customizer.configurationSource(corsConfigurationSource(origins)));
+
+        httpSecurity.headers(headers -> headers.httpStrictTransportSecurity(
+                httpStrictTransportSecurity ->
+                        httpStrictTransportSecurity.maxAgeInSeconds(Duration.ofDays(HSTS_MAX_AGE_DAYS).toSeconds())
+                                .includeSubDomains(true)
+                                .preload(true)
+                                .requestMatcher(AnyRequestMatcher.INSTANCE)));
+
+        httpSecurity.headers(headers -> headers.addHeaderWriter(new ReferrerPolicyHeaderWriter(
+                ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN)));
+
+        httpSecurity.headers(headers -> headers.addHeaderWriter(new PermissionsPolicyHeaderWriter(PERMISSION_POLICY)));
 
         httpSecurity.authorizeHttpRequests(auth -> auth
                 .requestMatchers(WHITELIST_PATHS)
@@ -81,17 +102,14 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated());
 
-        httpSecurity.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt()
-                        .jwtAuthenticationConverter(
-                                new JwtAuthenticationTokenConverter(resourceClient)))
-                .oauth2Client();
+        httpSecurity.oauth2ResourceServer(server -> server.jwt(custom -> custom.jwtAuthenticationConverter(new JwtAuthenticationTokenConverter(resourceClient))))
+                .oauth2Client(Customizer.withDefaults());
 
         return httpSecurity.build();
     }
 
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource(@Value("${cors.origins}") List<String> origins) {
+    private CorsConfigurationSource corsConfigurationSource(List<String> origins) {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(origins);
         configuration.setAllowedMethods(List.of("*"));
