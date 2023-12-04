7f2394febe41dc6364c796249202dea87abe5d60
asmierzchalski <adam.smierzchalski@doubleslash.de>
2023-12-04 15:13:20 +0100

    DO-1266 fixed testdata-b

--- tx-backend/src/main/resources/db/testdata-b/afterMigrate__011_asset_as_built_childs.sql
+++ tx-backend/src/main/resources/db/testdata-b/afterMigrate__011_asset_as_built_childs.sql
@@ -7,4 +7,4 @@ insert into assets_as_built_childs
 values
     -- supplier is child of owner
     (${assetAsBuiltId07}, ${assetAsBuiltId01}, 'H-LeftHeadLight'),  -- Z1 isParentOf Xenon Left-Headlights
-    (${assetAsBuiltId07}, ${assetAsBuiltId02}, 'H-RightHeadLight'), -- Z1 isParentOf Xenon Right-Headlights
+    (${assetAsBuiltId07}, ${assetAsBuiltId02}, 'H-RightHeadLight'); -- Z1 isParentOf Xenon Right-Headlights
