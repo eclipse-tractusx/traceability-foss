f755eee791b45310f92513083e6bedc20ecfc738
ashanmugavel <abilash.shanmugavel@doubleslash.com>
2023-12-14 14:48:50 +0100

    chore:[TRACEFOSS-XXX] changed KnownAdminRoutes name

--- frontend/src/app/modules/page/admin/core/admin.model.ts
+++ frontend/src/app/modules/page/admin/core/admin.model.ts
@@ -22,7 +22,7 @@
 import { CalendarDateModel } from '@core/model/calendar-date.model';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
-export enum KnownAdminRouts {
+export enum KnownAdminRoutes {
   REGISTRY = 'registry-lookups',
   BPN = 'configure-bpn',
   IMPORT = 'configure-import'
