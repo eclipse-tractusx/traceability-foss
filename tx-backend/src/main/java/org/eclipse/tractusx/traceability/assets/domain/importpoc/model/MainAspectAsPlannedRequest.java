98a0b9aa16f56cfba450f867c23c7f0b983dfa42
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-01-10 15:11:04 +0100

    feat(mapping): TRACEFOSS-2741 fixed bad structure import validation. Added bpn validation for assets to be imported matching app bpn.

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/domain/importpoc/model/MainAspectAsPlannedRequest.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/domain/importpoc/model/MainAspectAsPlannedRequest.java
@@ -1,12 +1,13 @@
 package org.eclipse.tractusx.traceability.assets.domain.importpoc.model;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public record MainAspectAsPlannedRequest(String catenaXId, ValidityPeriod validityPeriod,
                                          PartTypeInformation partTypeInformation
 ) {
 
-    public record ValidityPeriod(LocalDateTime validFrom, LocalDateTime validTo) {
+    public record ValidityPeriod(OffsetDateTime validFrom, OffsetDateTime validTo) {
     }
 
     public record PartTypeInformation(String manufacturerPartId, String classification, String nameAtManufacturer) {
