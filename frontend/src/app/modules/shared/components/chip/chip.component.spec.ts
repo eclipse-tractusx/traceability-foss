244c8549aa4c89ec6c79084b5a80251e7da41b75
Martin Maul <martin.maul@doubleslash.de>
2024-01-16 14:28:52 +0100

    feature(assetState): #556 added importNote textfield in detailed View

--- frontend/src/app/modules/shared/components/chip/chip.component.spec.ts
+++ frontend/src/app/modules/shared/components/chip/chip.component.spec.ts
@@ -1,23 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SharedModule } from '@shared/shared.module';
+import { renderComponent } from '@tests/test-render.utils';
 
 import { ChipComponent } from './chip.component';
 
 describe('ChipComponent', () => {
-  let component: ChipComponent;
-  let fixture: ComponentFixture<ChipComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ChipComponent ]
+  const renderChipComponent = () => {
+    return renderComponent(ChipComponent, {
+      imports: [SharedModule],
+
     })
-    .compileComponents();
+  }
 
-    fixture = TestBed.createComponent(ChipComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    const { fixture} = await renderChipComponent();
+    const { componentInstance } = fixture;
+    expect(componentInstance).toBeTruthy();
   });
 });
