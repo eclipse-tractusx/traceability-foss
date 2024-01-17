85a5a250f78f3f1656adc45582ce36325cf7016c
Pooja Patel <poojapatelce@gmail.com>
2024-01-17 16:36:01 +0100

    Revert name changes from Entity class and declare in SearchCriteriaRequestParam for filtering issue

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/request/SearchCriteriaRequestParam.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/request/SearchCriteriaRequestParam.java
@@ -117,6 +117,14 @@ public class SearchCriteriaRequestParam {
 
     private static String handleFilterParameter(final String filterParameter) {
         // As long as no clear spelling is defined, be lax with it. https://github.com/eclipse-tractusx/sldt-semantic-models/issues/470
-        return filterParameter.equalsIgnoreCase("catenaxsiteid") ? "catenaXSiteId" : filterParameter;
+        if (filterParameter.equalsIgnoreCase("catenaxsiteid")) {
+            return "catenaXSiteId";
+        } else if (filterParameter.equalsIgnoreCase("qualityAlertsInStatusActive")) {
+            return "noOfActiveAlerts";
+        }  else if (filterParameter.equalsIgnoreCase("qualityInvestigationsInStatusActive")) {
+            return "noOfActiveInvestigations";
+        }  else {
+            return filterParameter;
+        }
     }
 }
