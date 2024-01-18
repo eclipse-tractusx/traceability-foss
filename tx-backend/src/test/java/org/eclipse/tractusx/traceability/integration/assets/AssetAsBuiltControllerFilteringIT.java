06e86565c66b702ca64a6d89cacf119f21e7dc39
Martin Maul <martin.maul@doubleslash.de>
2024-01-18 10:55:32 +0100

    feature(assetState): #556 added support for importField searching/filtering/autocomplete

--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/assets/AssetAsBuiltControllerFilteringIT.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/assets/AssetAsBuiltControllerFilteringIT.java
@@ -20,6 +20,7 @@
 package org.eclipse.tractusx.traceability.integration.assets;
 
 import io.restassured.http.ContentType;
+import org.eclipse.tractusx.traceability.assets.domain.base.model.ImportState;
 import org.eclipse.tractusx.traceability.assets.infrastructure.asbuilt.model.AssetAsBuiltEntity;
 import org.eclipse.tractusx.traceability.assets.infrastructure.asbuilt.repository.JpaAssetAsBuiltRepository;
 import org.eclipse.tractusx.traceability.assets.infrastructure.base.model.AssetBaseEntity;
@@ -699,4 +700,27 @@ class AssetAsBuiltControllerFilteringIT extends IntegrationTestSpecification {
                 .body("totalItems", equalTo(13));
     }
 
+    @Test
+    void givenAssetsWithImportStateTransientExistent_whenCallFilteredEndpoint_thenReturnProperAssets() throws JoseException {
+        // given
+        assetsSupport.defaultAssetsStored();
+        AssetAsBuiltEntity assetAsBuilt = jpaAssetAsBuiltRepository.findById("urn:uuid:d387fa8e-603c-42bd-98c3-4d87fef8d2bb").orElseThrow();
+        assetAsBuilt.setImportState(ImportState.TRANSIENT);
+        jpaAssetAsBuiltRepository.save(assetAsBuilt);
+
+
+        final String filter = "?filter=importState,EQUAL,TRANSIENT,AND";
+
+        // then
+        given()
+                .header(oAuth2Support.jwtAuthorization(ADMIN))
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get("/api/assets/as-built" + filter)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("content.importState", equalTo(List.of("TRANSIENT")));
+    }
 }
