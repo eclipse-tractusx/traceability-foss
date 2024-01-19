e3842b7626fcb413319dfe961dcc2cb7c4e37821
Martin Maul <martin.maul@doubleslash.de>
2024-01-19 14:00:00 +0100

    feature(activeNotification): 415 remove activeAlert/inInvestigation from FE

--- tx-backend/src/main/resources/db/migration/V10__remove_active_notifications.sql
+++ tx-backend/src/main/resources/db/migration/V10__remove_active_notifications.sql
@@ -1,3 +1,6 @@
+/* Delete View with relations to removed fields */
+DROP VIEW assets_as_built_view;
+
 /* Remove fields which are not needed anymore */
 ALTER TABLE public.assets_as_built DROP COLUMN IF EXISTS active_alert;
 ALTER TABLE public.assets_as_built DROP COLUMN IF EXISTS in_investigation;
