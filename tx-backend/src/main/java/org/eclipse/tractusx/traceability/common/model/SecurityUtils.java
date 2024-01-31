9a4b870560be8113f1cc844489e30ba6a7567ea6
ds-lcapellino <lucas.capellino@doubleslash.de>
2024-01-31 10:25:02 +0100

    chore: update spring version

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/model/SecurityUtils.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/common/model/SecurityUtils.java
@@ -20,7 +20,7 @@
 package org.eclipse.tractusx.traceability.common.model;
 
 
-import org.apache.commons.text.StringEscapeUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.eclipse.tractusx.traceability.qualitynotification.infrastructure.edc.model.EDCNotification;
 import org.eclipse.tractusx.traceability.qualitynotification.infrastructure.edc.model.EDCNotificationContent;
 import org.eclipse.tractusx.traceability.qualitynotification.infrastructure.edc.model.EDCNotificationHeader;
