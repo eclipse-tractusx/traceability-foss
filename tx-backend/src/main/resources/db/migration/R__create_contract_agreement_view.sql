62b810628708b4089706b2fc48f81a55762a8f74
ds-lcapellino <lucas.capellino@doubleslash.de>
2024-03-06 16:43:04 +0100

    feature: 420 not use null values in contract_agreement_info_view.contract_agreement_id

--- tx-backend/src/main/resources/db/migration/R__create_contract_agreement_view.sql
+++ tx-backend/src/main/resources/db/migration/R__create_contract_agreement_view.sql
@@ -1,8 +1,11 @@
 create or replace view contract_agreement_info_view as
 SELECT *
-FROM ((SELECT assets_as_built.id, contract_agreement_id, 'assets_as_built' as asset_type, created FROM assets_as_built)
+FROM ((SELECT assets_as_built.id, contract_agreement_id, 'assets_as_built' as asset_type, created
+       FROM assets_as_built
+       where contract_agreement_id is not null)
       UNION ALL
       (SELECT assets_as_planned.id, contract_agreement_id, 'assets_as_planned' as asset_type, created
-       FROM assets_as_planned)) results
+       FROM assets_as_planned
+       where contract_agreement_id is not null)) results
 ORDER BY created DESC;
 
