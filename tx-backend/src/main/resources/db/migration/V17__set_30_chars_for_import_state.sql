bbf47d1b96ebb926743601852c414ba1bbbb8a0e
ds-ext-sceronik <sebastian.ceronik@partner.doubleslash.de>
2024-03-14 15:29:10 +0100

    feature(tx-backend): #536 fix migration

--- tx-backend/src/main/resources/db/migration/V17__set_30_chars_for_import_state.sql
+++ tx-backend/src/main/resources/db/migration/V17__set_30_chars_for_import_state.sql
@@ -1,2 +1,3 @@
+DROP VIEW IF EXISTS assets_as_built_view;
 ALTER TABLE assets_as_planned ALTER COLUMN import_state TYPE VARCHAR (30);
 ALTER TABLE assets_as_built ALTER COLUMN import_state TYPE VARCHAR(30);
