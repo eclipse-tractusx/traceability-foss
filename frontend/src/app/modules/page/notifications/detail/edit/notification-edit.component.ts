0246abfa13d95b46a42ce2251cabce10d7d3a57f
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-04-05 22:22:12 +0200

    chore(notifications): 622 - added functionality to add affected parts.

--- frontend/src/app/modules/page/notifications/detail/edit/notification-edit.component.ts
+++ frontend/src/app/modules/page/notifications/detail/edit/notification-edit.component.ts
@@ -21,30 +21,23 @@ import { AfterViewInit, Component, OnDestroy, TemplateRef, ViewChild } from '@an
 import { ActivatedRoute, Router } from '@angular/router';
 import { getRoute, NOTIFICATION_BASE_ROUTE } from '@core/known-route';
 import { Pagination } from '@core/model/pagination.model';
+import { DEFAULT_PAGE_SIZE, FIRST_PAGE } from '@core/pagination/pagination.model';
 import { NotificationDetailFacade } from '@page/notifications/core/notification-detail.facade';
-import { NotificationHelperService } from '@page/notifications/core/notification-helper.service';
 import { NotificationsFacade } from '@page/notifications/core/notifications.facade';
 import { PartsFacade } from '@page/parts/core/parts.facade';
+import { MainAspectType } from '@page/parts/model/mainAspectType.enum';
 import { Part } from '@page/parts/model/parts.model';
 import { NotificationActionHelperService } from '@shared/assembler/notification-action-helper.service';
+import { TableType } from '@shared/components/multi-select-autocomplete/table-type.model';
 import { NotificationCommonModalComponent } from '@shared/components/notification-common-modal/notification-common-modal.component';
-import {
-  CreateHeaderFromColumns,
-  TableConfig,
-  TableEventConfig,
-  TableHeaderSort,
-} from '@shared/components/table/table.model';
+import { TableHeaderSort } from '@shared/components/table/table.model';
 import { ToastService } from '@shared/components/toasts/toast.service';
-import { containsAtleastOneFilterEntry, toAssetFilter } from '@shared/helper/filter-helper';
-import { Notification, NotificationStatus, NotificationType } from '@shared/model/notification.model';
+import { toAssetFilter } from '@shared/helper/filter-helper';
+import { Notification } from '@shared/model/notification.model';
 import { View } from '@shared/model/view.model';
-import { NotificationAction } from '@shared/modules/notification/notification-action.enum';
-import { PartDetailsFacade } from '@shared/modules/part-details/core/partDetails.facade';
 import { StaticIdService } from '@shared/service/staticId.service';
 import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
-import { filter, first, tap } from 'rxjs/operators';
-import {TableType} from "@shared/components/multi-select-autocomplete/table-type.model";
-import {MainAspectType} from "@page/parts/model/mainAspectType.enum";
+import { first, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-notification-edit',
@@ -56,30 +49,26 @@ export class NotificationEditComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('semanticModelIdTmp') semanticModelIdTmp: TemplateRef<unknown>;
   public readonly partsAsBuilt$: Observable<View<Pagination<Part>>>;
+  public affectedParts: Part[];
+  private filteredDataCache: any[] = [];
+
   public readonly titleId = this.staticIdService.generateId('NotificationDetail');
   public readonly deselectPartTrigger$ = new Subject<Part[]>();
   public readonly editMode = true;
-  public readonly notificationPartsInformation$: Observable<View<Part[]>>;
-  public readonly supplierPartsDetailInformation$: Observable<View<Part[]>>;
+
   public readonly selected$: Observable<View<Notification>>;
 
-  public affectedParts = [];
-  public readonly selectedItems$ = new BehaviorSubject<Part[]>([]);
+  public affectedPartIds: string[] = [];
+
+  public temporaryAffectedParts: string[] = [];
+
   public readonly addPartTrigger$ = new Subject<Part>();
   public readonly currentSelectedAvailableParts$ = new BehaviorSubject<Part[]>([]);
   public readonly currentSelectedAffectedParts$ = new BehaviorSubject<Part[]>([]);
-  public readonly isAlertOpen$ = new BehaviorSubject<boolean>(false);
 
-  public readonly notificationPartsTableId = this.staticIdService.generateId('NotificationDetail');
-
-  public notificationPartsTableConfig: TableConfig;
-  public supplierPartsTableConfig: TableConfig;
-  public isReceived: boolean;
   private originPageNumber: number;
   private originTabIndex: number;
 
-
-  private selectedNotificationTmpStore: Notification;
   public selectedNotification: Notification;
   public tableAsBuiltSortList: TableHeaderSort[];
   private paramSubscription: Subscription;
@@ -96,20 +85,20 @@ export class NotificationEditComponent implements AfterViewInit, OnDestroy {
   ) {
     this.partsAsBuilt$ = this.partsFacade.partsAsBuilt$;
     this.partsFacade.setPartsAsBuilt();
-    this.notificationPartsInformation$ = this.notificationDetailFacade.notificationPartsInformation$;
-    this.supplierPartsDetailInformation$ = this.notificationDetailFacade.supplierPartsInformation$;
     this.selected$ = this.notificationDetailFacade.selected$;
-console.log(this.selectedNotification, "selected");
+
 
     this.currentSelectedAvailableParts$.subscribe((parts: Part[]) => {
-      // Do something with the emitted array of parts
       console.log('Received available:', parts);
-      // Example: Update UI or perform other operations
+      this.temporaryAffectedParts = parts.map(part => part.id);
+      console.log(this.temporaryAffectedParts, 'temp affected');
     });
 
     this.currentSelectedAffectedParts$.subscribe((parts: Part[]) => {
       // Do something with the emitted array of parts
       console.log('Received affected:', parts);
+      this.temporaryAffectedParts = parts.map(part => part.id);
+      console.log(this.temporaryAffectedParts, 'temp affected');
       // Example: Update UI or perform other operations
     });
     this.paramSubscription = this.route.queryParams.subscribe(params => {
@@ -119,66 +108,35 @@ console.log(this.selectedNotification, "selected");
     });
 
 
-
   }
 
   // TODO parts table
   public onSelectedItemAvailableParts($event: Record<string, unknown>): void {
-  /*  this.partDetailsFacade.selectedPart = $event as unknown as Part;
-    let tableData = {};
-    for (let component of this.partsTableComponents) {
-      tableData[component.tableType + "_PAGE"] = component.pageIndex;
-    }
-    this.router.navigate([`parts/${$event?.id}`], {queryParams: tableData})*/
+    /*  this.partDetailsFacade.selectedPart = $event as unknown as Part;
+      let tableData = {};
+      for (let component of this.partsTableComponents) {
+        tableData[component.tableType + "_PAGE"] = component.pageIndex;
+      }
+      this.router.navigate([`parts/${$event?.id}`], {queryParams: tableData})*/
   }
 
   public onSelectedItemAffectedParts($event: Record<string, unknown>): void {
-console.log("affected", $event)
-  }
-
-
-  // TODO parts table
-  public onAsBuiltTableConfigChange({page, pageSize, sorting}: TableEventConfig): void {
-/*
-    this.setTableSortingList(sorting, MainAspectType.AS_BUILT);
-    this.currentPartTablePage['AS_BUILT_OWN_PAGE'] = page;
-    let pageSizeValue = this.DEFAULT_PAGE_SIZE;
-    if (pageSize !== 0) {
-      pageSizeValue = pageSize;
-    }
-    if (this.assetAsBuiltFilter && containsAtleastOneFilterEntry(this.assetAsBuiltFilter)) {
-      this.partsFacade.setPartsAsBuilt(0, pageSizeValue, this.tableAsBuiltSortList, toAssetFilter(this.assetAsBuiltFilter, true));
-    } else {
-      this.partsFacade.setPartsAsBuilt(page, pageSizeValue, this.tableAsBuiltSortList);
-    }
-*/
+    console.log('affected', $event);
 
   }
-  // TODO parts table
-  filterActivated(isAsBuilt: boolean, assetFilter: any): void {
-/*    if (isAsBuilt) {
-      this.assetAsBuiltFilter = assetFilter;
-      this.partsFacade.setPartsAsBuilt(this.currentPartTablePage['AS_BUILT_OWN_PAGE'] ?? 0, this.DEFAULT_PAGE_SIZE, this.tableAsBuiltSortList, toAssetFilter(this.assetAsBuiltFilter, true));
-    } else {
-      this.assetsAsPlannedFilter = assetFilter;
-      this.partsFacade.setPartsAsPlanned(this.currentPartTablePage['AS_PLANNED_OWN_PAGE'] ?? 0, this.DEFAULT_PAGE_SIZE, this.tableAsPlannedSortList, toAssetFilter(this.assetsAsPlannedFilter, false));
-    }*/
+
+
+  filterAffectedAssets(assetFilter: any): void {
   }
 
+  filterAvailableAssets(assetFilter: any): void {
+    this.partsFacade.setPartsAsBuilt(FIRST_PAGE, DEFAULT_PAGE_SIZE, this.tableAsBuiltSortList, toAssetFilter(assetFilter, true));
+  }
 
 
   // TODO implement save / detection change
   public clickedSave(): void {
 
-  }
-
-  // TODO Implement cancel / popup do you really want to leave without saving?
-  public clickedCancellation(): void {
-
-  }
-
-  public submittedByNotificationRequest(): void{
-    console.log("submitted");
   }
 
   public ngAfterViewInit(): void {
@@ -193,47 +151,51 @@ console.log("affected", $event)
     this.paramSubscription?.unsubscribe();
   }
 
-  removeSelectedPartsToAffectedParts(){
+  filterDataMemoized = (() => {
+    const memo = new Map<string, any[]>();
+
+    return (data: any[]): any[] => {
+      const key = JSON.stringify(data); // Use data as cache key
+
+      // Check if the result is already memoized
+      if (memo.has(key)) {
+        return memo.get(key)!;
+      } else {
+        // Perform filtering operation
+        const filteredData = data.filter(part => this.affectedPartIds.includes(part.id));
+
+        // Cache the result
+        memo.set(key, filteredData);
+        return filteredData;
+      }
+    };
+  })();
+
+  filterOnlyAffected(parts: any): Pagination<Part> {
+    console.log('Incoming parts', parts.content);
+    console.log('affectedPartIds', this.affectedPartIds);
+
+    // TODO performance
+    const partsFiltered = parts.content.filter(part => this.affectedPartIds.includes(part.id));
+
+    // TODO pagination
+    return {
+      page: 0,
+      pageCount: 0,
+      pageSize: 0,
+      totalItems: 0,
+      content: partsFiltered,
+    };
+  }
+
+  removeSelectedPartsToAffectedParts() {
 
   }
 
-  addSelectedPartsToAffectedParts(){
-
-  }
-  public onNotificationPartsSort({ sorting }: TableEventConfig): void {
-    const [ name, direction ] = sorting || [ '', '' ];
-    this.notificationDetailFacade.sortNotificationParts(name, direction);
+  addSelectedPartsToAffectedParts() {
+    this.temporaryAffectedParts.forEach(value => this.affectedPartIds.push(value));
   }
 
-  public onSupplierPartsSort({ sorting }: TableEventConfig): void {
-    const [ name, direction ] = sorting || [ '', '' ];
-    this.notificationDetailFacade.sortSupplierParts(name, direction);
-  }
-
-  public onMultiSelect(event: unknown[]): void {
-    this.selectedNotificationTmpStore = Object.assign(this.notificationDetailFacade.selected);
-    this.selectedItems$.next(event as Part[]);
-  }
-
-  public removeItemFromSelection(part: Part): void {
-    this.deselectPartTrigger$.next([ part ]);
-    this.selectedItems$.next(this.selectedItems$.getValue().filter(({ id }) => id !== part.id));
-  }
-
-  public clearSelected(): void {
-    this.deselectPartTrigger$.next(this.selectedItems$.getValue());
-    this.selectedItems$.next([]);
-  }
-
-  public addItemToSelection(part: Part): void {
-    this.addPartTrigger$.next(part);
-    this.selectedItems$.next([ ...this.selectedItems$.getValue(), part ]);
-  }
-
-  public copyToClipboard(semanticModelId: string): void {
-    const text = { id: 'clipboard', values: { value: semanticModelId } };
-    navigator.clipboard.writeText(semanticModelId).then(_ => this.toastService.info(text));
-  }
 
   public navigateBackToNotifications(): void {
     const { link } = getRoute(NOTIFICATION_BASE_ROUTE);
@@ -245,56 +207,21 @@ console.log("affected", $event)
     });
   }
 
-  public handleConfirmActionCompletedEvent(): void {
-    this.notificationDetailFacade.selected = { loader: true };
-    this.ngAfterViewInit();
-  }
-
-  private setTableConfigs(data: Notification): void {
-    this.isReceived = !data.isFromSender;
-
-    const displayedColumns = [ 'id', 'semanticDataModel', 'name', 'semanticModelId' ];
-    const sortableColumns = { id: true, semanticDataModel: true, name: true, semanticModelId: true };
-
-    const tableConfig = {
-      displayedColumns,
-      header: CreateHeaderFromColumns(displayedColumns, 'table.column'),
-      sortableColumns: sortableColumns,
-      hasPagination: false,
-      cellRenderers: {
-        semanticModelId: this.semanticModelIdTmp,
-      },
-    };
-
-    this.notificationDetailFacade.setNotificationPartsInformation(data);
-    this.notificationPartsTableConfig = { ...tableConfig };
-
-    if (!this.isReceived) {
-      return;
-    }
-    this.notificationDetailFacade.setAndSupplierPartsInformation();
-    this.supplierPartsTableConfig = {
-      ...tableConfig,
-      displayedColumns: [ 'select', ...displayedColumns ],
-      header: CreateHeaderFromColumns([ 'select', ...displayedColumns ], 'table.column'),
-    };
-  }
-
   private selectedNotificationBasedOnUrl(): void {
     const notificationId = this.route.snapshot.paramMap.get('notificationId');
+
     this.notificationsFacade
       .getNotification(notificationId)
       .pipe(
         first(),
-        tap(notification => (this.notificationDetailFacade.selected = { data: notification })),
+        tap(notification => {
+          this.notificationDetailFacade.selected = { data: notification };
+          this.affectedPartIds = notification.assetIds;
+        }),
       )
       .subscribe();
   }
 
-  protected readonly NotificationType = NotificationType;
-  protected readonly NotificationAction = NotificationAction;
-
-  protected readonly NotificationStatus = NotificationStatus;
-    protected readonly TableType = TableType;
-    protected readonly MainAspectType = MainAspectType;
+  protected readonly TableType = TableType;
+  protected readonly MainAspectType = MainAspectType;
 }
--- frontend/src/app/modules/shared/components/request-notification-new/notification-new-request.component.ts
+++ frontend/src/app/modules/shared/components/request-notification-new/notification-new-request.component.ts
@@ -17,89 +17,69 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { bpnRegex } from '@page/admin/presentation/bpn-configuration/bpn-configuration.component';
-import { Part } from '@page/parts/model/parts.model';
+import { NotificationDetailFacade } from '@page/notifications/core/notification-detail.facade';
 import { BaseInputHelper } from '@shared/abstraction/baseInput/baseInput.helper';
-import { DateValidators } from '@shared/components/dateTime/dateValidators.model';
-import { ToastService } from '@shared/components/toasts/toast.service';
-import {Notification, NotificationType} from '@shared/model/notification.model';
+import { Notification, NotificationType } from '@shared/model/notification.model';
 import { Severity } from '@shared/model/severity.model';
-import { NotificationService } from '@shared/service/notification.service';
-import {BehaviorSubject, Observable, Subscription} from 'rxjs';
-import {filter, tap} from "rxjs/operators";
-import {NotificationDetailFacade} from "@page/notifications/core/notification-detail.facade";
-import {View} from "@shared/model/view.model";
+import { View } from '@shared/model/view.model';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-notification-new-request',
   templateUrl: './notification-new-request.component.html',
 })
-export class RequestNotificationNewComponent implements  AfterViewInit, OnDestroy{
+export class RequestNotificationNewComponent implements OnDestroy, OnInit {
   @Input() title: string;
-  @Input() selectedItems: Part[];
-  private subscription: Subscription;
   @Input() editMode: boolean;
-  formGroup = new FormGroup<any>({});
+
+
+  public readonly formGroup = new FormGroup<any>({
+    'title': new FormControl('', [ Validators.maxLength(30), Validators.minLength(0) ]),
+    'description': new FormControl('', [ Validators.required, Validators.maxLength(1000), Validators.minLength(15) ]),
+    'severity': new FormControl(Severity.MINOR, [ Validators.required ]),
+    'targetDate': new FormControl(null),
+    'bpn': new FormControl(null, [ Validators.required, BaseInputHelper.getCustomPatternValidator(bpnRegex, 'bpn') ]),
+    'type': new FormControl(NotificationType.INVESTIGATION, [ Validators.required ]),
+  });
   public readonly selected$: Observable<View<Notification>>;
-  @Output() submitted = new EventEmitter<void>();
 
   public readonly isLoading$ = new BehaviorSubject(false);
   public readonly minDate = new Date();
-  public selectedNotification: Notification;
-  constructor(public readonly notificationDetailFacade: NotificationDetailFacade,) {
+
+  private subscription: Subscription;
+
+  constructor(public readonly notificationDetailFacade: NotificationDetailFacade) {
     this.selected$ = this.notificationDetailFacade.selected$;
-    this.formGroup.addControl('title', new FormControl('', [ Validators.maxLength(30), Validators.minLength(0) ]));
-    this.formGroup.addControl('description', new FormControl('', [ Validators.required, Validators.maxLength(1000), Validators.minLength(15) ]));
-    this.formGroup.addControl('severity', new FormControl(Severity.MINOR, [ Validators.required ]));
-    this.formGroup.addControl('targetDate', new FormControl(null, []));
-    this.formGroup.addControl('bpn', new FormControl(null, [ Validators.required, BaseInputHelper.getCustomPatternValidator(bpnRegex, 'bpn') ]));
-    this.formGroup.addControl('type', new FormControl(NotificationType.INVESTIGATION, [ Validators.required ]));
-
-    this.formGroup.valueChanges.subscribe((data: any) => {
-      console.log('Form has been changed', data);
-      // You can perform any action here when the form is changed
-    });
-
-
   }
+
+  ngOnInit(): void {
+
+    this.subscription = this.selected$
+      .pipe(
+        filter(({ data }) => !!data),
+        tap(({ data }) => {
+          const { title, description, severity, type, sendTo, targetDate } = data;
+          this.formGroup.setValue({
+            'title': title,
+            'description': description,
+            'severity': severity,
+            'type': type,
+            'bpn': sendTo,
+            'targetDate': targetDate.isInitial() ? null : targetDate.valueOf().toISOString().slice(0, 16),
+          });
+          if (this.editMode) {
+            this.formGroup.get('type').disable();
+          }
+        }),
+      )
+      .subscribe();
+  }
+
   public ngOnDestroy(): void {
     this.subscription?.unsubscribe();
   }
-
-  public ngAfterViewInit(): void {
-    if (this.editMode){
-      this.formGroup.get('type').disable();
-    }
-    this.subscription = this.selected$
-        .pipe(
-            filter(({ data }) => !!data),
-            tap(({ data }) => {
-              // TODO check if we need to set it after form change to the parent comp
-           //   this.setTableConfigs(data);
-              this.selectedNotification = data;
-              this.formGroup.controls['title'].setValue(data.title);
-              this.formGroup.controls['description'].setValue(data.description);
-              this.formGroup.controls['severity'].setValue(data.severity);
-              this.formGroup.controls['type'].setValue(data.type);
-              this.formGroup.controls['bpn'].setValue(data.sendTo);
-              if (!data.targetDate.isInitial()){
-                this.formGroup.controls['targetDate'].patchValue(data.targetDate.valueOf().toISOString().slice(0, 16));
-              }
-            }),
-        )
-        .subscribe();
-  }
-
-  protected prepareSubmit(): void {
-    this.formGroup.markAllAsTouched();
-    this.formGroup.updateValueAndValidity();
-
-    if (this.formGroup.invalid) return;
-
-    this.isLoading$.next(true);
-    this.formGroup.disable();
-  }
-
 }
