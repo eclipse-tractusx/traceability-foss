21a6ba73b00a8c10a0e32f83ac3e4d65579f1335
ds-lcapellino <lucas.capellino@doubleslash.de>
2024-04-19 09:12:57 +0200

    bug: #828 fix flyway scripts

--- tx-backend/src/main/resources/db/migration/V22__add_asset_as_built_fk_to_tractionbatterycode.sql
+++ tx-backend/src/main/resources/db/migration/V22__add_asset_as_built_fk_to_tractionbatterycode.sql
@@ -3,8 +3,5 @@ ALTER TABLE traction_battery_code_subcomponent
 ALTER TABLE traction_battery_code_subcomponent
     ADD CONSTRAINT fk_asset_traction_battery_code_subcomponent FOREIGN KEY (asset_as_built_id) REFERENCES assets_as_built (id) on delete cascade;
 
-ALTER VIEW IF EXISTS assets_as_built_view
-    ALTER COLUMN traction_battery_code DROP DEFAULT;
-
 ALTER TABLE assets_as_built
-    DROP COLUMN traction_battery_code;
+    DROP COLUMN traction_battery_code CASCADE;
