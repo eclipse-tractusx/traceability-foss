f071f049344e5206308268d62b4218a54506c789
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-05-06 09:49:47 +0200

    chore(validation): 783 reject notification when sender is receiver (BPN)

--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/edc/blackbox/EdcControllerIT.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/edc/blackbox/EdcControllerIT.java
@@ -65,7 +65,7 @@ class EdcControllerIT extends IntegrationTestSpecification {
     }
 
     @Test
-    void shouldCreateAnInvestigationIncludingNotificationOnAPICallClass() throws IOException, JoseException {
+    void shouldRejectInvestigationNotificationOnAPICallClass_whenAppBpnEqualsSenderBpn() throws IOException, JoseException {
         // given
         assetsSupport.defaultAssetsStored();
         String notificationJson = readFile("/testdata/edc_notification_okay.json");
@@ -79,12 +79,11 @@ class EdcControllerIT extends IntegrationTestSpecification {
                 .when()
                 .post("/api/qualitynotifications/receive")
                 .then()
-                .statusCode(200);
+                .statusCode(400);
 
         // then
-        investigationNotificationsSupport.assertNotificationsSize(1);
-        investigationsSupport.assertInvestigationsSize(1);
-        investigationsSupport.assertInvestigationStatus(NotificationStatus.RECEIVED);
+        investigationNotificationsSupport.assertNotificationsSize(0);
+        investigationsSupport.assertInvestigationsSize(0);
     }
 
     @Test
