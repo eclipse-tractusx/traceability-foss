99c4dc95ea5aefad5d8ebd7b2a1d53f8e38ab96a
Maximilian Wesener <124587888+ds-mwesener@users.noreply.github.com>
2024-05-13 08:25:38 +0200

    Update frontend/src/app/modules/shared/modules/notification/modal/actions/notification-action-modal.component.spec.ts
    Co-authored-by: Lucas Capellino <137265091+ds-lcapellino@users.noreply.github.com>

--- frontend/src/app/modules/shared/modules/notification/modal/actions/notification-action-modal.component.spec.ts
+++ frontend/src/app/modules/shared/modules/notification/modal/actions/notification-action-modal.component.spec.ts
@@ -1,7 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2022, 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
- * Copyright (c) 2022, 2023 ZF Friedrichshafen AG
- * Copyright (c) 2022, 2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
