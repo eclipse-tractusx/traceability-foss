19f2d6f2acdde02c86d22eb0cf1fe0d5b208af46
ashanmugavel <abilash.shanmugavel@doubleslash.com>
2024-06-11 10:28:36 +0200

    chore-xxx: fixed jsonSchemaTest

--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/JsonSchemaTest.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/JsonSchemaTest.java
@@ -120,6 +120,8 @@ public class JsonSchemaTest {
                     }
                 }
             }
+        } catch (NumberFormatException e) {
+            log.info("error:" + e);
         }
         return highestVersionFile;
     }
