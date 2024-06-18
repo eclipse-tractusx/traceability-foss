341d14b1e5330d678fa19b1c93360771b6b2ffea
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-06-18 12:17:12 +0200

    feature(refactoring):xxx fixed db migration

---
+++ tx-backend/src/main/resources/db/migration/V23__change_notification_model.sql
@@ -0,0 +1,11 @@
+ALTER TABLE notification
+    DROP COLUMN IF EXISTS accept_reason,
+    DROP COLUMN IF EXISTS decline_reason,
+    DROP COLUMN IF EXISTS close_reason,
+    ADD COLUMN target_date timestamp NULL,
+    ADD COLUMN severity int4 NULL;
+
+ALTER TABLE notification_message
+    DROP COLUMN IF EXISTS target_date,
+    DROP COLUMN IF EXISTS severity,
+    ADD COLUMN message varchar(1000) NULL;
