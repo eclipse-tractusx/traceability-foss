4cb78524429db7f2d891f97be571c803d4e55254
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-06-24 08:16:43 +0200

    feature(chore):962 fixed nullpointer

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/notification/infrastructure/notification/model/NotificationEntity.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/notification/infrastructure/notification/model/NotificationEntity.java
@@ -98,7 +98,7 @@ public class NotificationEntity extends NotificationBaseEntity {
                 .targetDate(convertInstantToString(notificationEntity.getTargetDate()))
                 .severity(NotificationSeverity.fromString(notificationEntity.getSeverity() != null ? notificationEntity.getSeverity().getRealName() : null))
                 .notifications(messages)
-                .initialReceiverBpns(List.of())
+                .initialReceiverBpns(List.of(initialReceiverBpn))
                 .build();
     }
 
