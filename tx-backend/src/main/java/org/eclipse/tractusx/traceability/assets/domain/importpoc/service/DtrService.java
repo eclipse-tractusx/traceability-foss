5f7ece2a1f823d6a247b8753216ece22811f58c7
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-07-03 16:17:36 +0200

    feature(chore):884 - adapted mapper / testdata.

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/domain/importpoc/service/DtrService.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/assets/domain/importpoc/service/DtrService.java
@@ -19,8 +19,6 @@
 
 package org.eclipse.tractusx.traceability.assets.domain.importpoc.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.tractusx.irs.component.assetadministrationshell.AssetAdministrationShellDescriptor;
