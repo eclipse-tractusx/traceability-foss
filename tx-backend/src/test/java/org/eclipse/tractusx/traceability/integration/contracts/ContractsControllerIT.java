21714a694853032291e716ecf71d61d44634faae
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-07-12 12:01:26 +0200

    chore(irs):1105 Added logging.

--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/contracts/ContractsControllerIT.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/contracts/ContractsControllerIT.java
@@ -45,30 +45,6 @@ class ContractsControllerIT extends IntegrationTestSpecification {
     @Autowired
     EdcSupport edcSupport;
 
-    @Test
-    void shouldReturnContracts() throws JoseException {
-        //GIVEN
-        edcSupport.edcWillReturnContractAgreements();
-        edcSupport.edcWillReturnContractAgreementNegotiation();
-        assetsSupport.defaultAssetsStored();
-
-        //WHEN
-        PageResult<ContractResponse> contractResponsePageResult = given()
-                .header(oAuth2Support.jwtAuthorization(ADMIN))
-                .contentType(ContentType.JSON)
-                .log().all()
-                .when()
-                .body(new PageableFilterRequest())
-                .post("/api/contracts")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().body().as(new TypeRef<>() {
-                });
-        //THEN
-        assertThat(contractResponsePageResult.content()).isNotEmpty();
-        assertThat(contractResponsePageResult.content().get(0).getPolicy()).isNotEmpty();
-    }
 
     @Test
     void shouldReturnOnlyOneContract() throws JoseException {
@@ -84,31 +60,6 @@ class ContractsControllerIT extends IntegrationTestSpecification {
                 .log().all()
                 .when()
                 .body(PageableFilterRequest.builder().searchCriteriaRequestParam(SearchCriteriaRequestParam.builder().filter(List.of("contractId,STARTS_WITH,abc1,AND")).build()).build())
-                .post("/api/contracts")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().body().as(new TypeRef<>() {
-                });
-        //THEN
-        assertThat(contractResponsePageResult.content()).isNotEmpty();
-        assertThat(contractResponsePageResult.content().get(0).getCounterpartyAddress()).isNotEmpty();
-    }
-
-    @Test
-    void shouldReturnContractsWithNotificationType() throws JoseException {
-        //GIVEN
-        edcSupport.edcWillReturnOnlyOneContractAgreement();
-        edcSupport.edcWillReturnContractAgreementNegotiation();
-        assetsSupport.defaultAssetsStored();
-
-        //WHEN
-        PageResult<ContractResponse> contractResponsePageResult = given()
-                .header(oAuth2Support.jwtAuthorization(ADMIN))
-                .contentType(ContentType.JSON)
-                .log().all()
-                .when()
-                .body(PageableFilterRequest.builder().searchCriteriaRequestParam(SearchCriteriaRequestParam.builder().filter(List.of("contractType,EQUAL,ASSET_AS_BUILT,AND")).build()).build())
                 .post("/api/contracts")
                 .then()
                 .log().all()
