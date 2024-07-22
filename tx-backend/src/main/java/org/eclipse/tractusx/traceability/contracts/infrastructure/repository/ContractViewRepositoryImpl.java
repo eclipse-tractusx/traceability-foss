2d3d98c962a71dba356c2fec4fab40a8ce6da34f
Maximilian Wesener <maximilian.wesener@doubleslash.de>
2024-07-22 16:52:02 +0200

    chore(update): 1105 - fix filter.

--- tx-backend/src/main/java/org/eclipse/tractusx/traceability/contracts/infrastructure/repository/ContractViewRepositoryImpl.java
+++ tx-backend/src/main/java/org/eclipse/tractusx/traceability/contracts/infrastructure/repository/ContractViewRepositoryImpl.java
@@ -73,7 +73,8 @@ public class ContractViewRepositoryImpl extends ContractRepositoryImplBase imple
             List<Contract> contracts = fetchEdcContractAgreements(baseEntities)
                     .stream()
                     .filter(contract -> contract.getContractId() != null)
-                    .filter(contract -> contract.getGlobalAssetId() != null && !contract.getType().equals(ContractType.NOTIFICATION)).toList();
+                    .filter(contract -> !(contract.getGlobalAssetId() == null && contract.getType().equals(ContractType.NOTIFICATION)))
+                    .toList();
 
             return new PageResult<>(contracts,
                     contractAgreementEntities.getPageable().getPageNumber(),
