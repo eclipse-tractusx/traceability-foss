cfa745e3aec327413f26156c6c12bb05385a7cb1
Jaro Hartmann <57985712+ds-jhartmann@users.noreply.github.com>
2024-10-01 14:45:06 +0200

    fix: [TRX-30] add data seeding on startup (#59)

--- tx-backend/src/main/resources/db/migration/R__create_asset_as_built_view.sql
+++ tx-backend/src/main/resources/db/migration/R__create_asset_as_built_view.sql
@@ -1,4 +1,50 @@
 -- ${flyway:timestamp}
+-- TODO remove after demo / discovery is working
+INSERT INTO assets_as_built (id,
+                             idShort,
+                             owner,
+                             classification,
+                             qualityType,
+                             manufacturerPartId,
+                             manufacturerId,
+                             manufacturerName,
+                             nameAtManufacturer,
+                             semanticDataModel,
+                             semanticModelId,
+                             van,
+                             importState,
+                             importNote,
+                             policyId,
+                             tombstone,
+                             manufacturingDate,
+                             manufacturingCountry,
+                             nameAtCustomer,
+                             customerPartId,
+                             productType)
+VALUES ('urn:uuid:6b2296cc-26c0-4f38-8a22-092338c36e22', -- id
+        NULL, -- idShort
+        'SUPPLIER', -- owner (Enum)
+        NULL, -- classification
+        NULL, -- qualityType (Enum)
+        '3500076-05', -- manufacturerPartId
+        'BPNL000000002BR4', -- manufacturerId
+        NULL, -- manufacturerName
+        'a/dev Vehicle Hybrid', -- nameAtManufacturer
+        'SERIALPART', -- semanticDataModel (Enum)
+        'OMAOYGBDTSRCMYSCX', -- semanticModelId
+        'OMAOYGBDTSRCMYSCX', -- van
+        'PERSISTENT', -- importState (Enum)
+        NULL, -- importNote
+        NULL, -- policyId
+        NULL, -- tombstone
+        '2018-09-28T04:15:57.000Z', -- manufacturingDate (Instant)
+        'DEU', -- manufacturingCountry
+        NULL, -- nameAtCustomer
+        NULL, -- customerPartId
+        NULL -- productType
+       )
+ON CONFLICT (id) DO NOTHING;
+
 create
 or replace view assets_as_built_view as
 select asset.*,
