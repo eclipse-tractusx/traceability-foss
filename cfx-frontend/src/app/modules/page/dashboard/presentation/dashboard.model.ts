22376e718c48bf5031504a2e7b366d9b94f518cd
ds-lcapellino <lucas.capellino@doubleslash.de>
2025-02-04 10:00:56 +0100

    chore: tracex-385 remove custom logic

--- cfx-frontend/src/app/modules/page/dashboard/presentation/dashboard.model.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface MetricData {
-  metricName: string,
-  value: Observable<number>,
-  metricUnit: string,
-}
--- cfx-frontend/src/app/modules/page/investigations/core/investigationsType.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum InvestigationsType {
-  RECEIVED = "Received",
-  QUEUED_AND_REQUESTED = "Queued and requested"
-}
