d8b479881a64b9d09cfa018b014efd0ae0941b0f
Eirik Strøm <eirik.strom@cofinity-x.com>
2025-02-11 10:55:38 +0100

    chore(TRACEX-278): Add env vars for injection (#240)

--- cfx-frontend/scripts/inject-dynamic-env.js
+++ cfx-frontend/scripts/inject-dynamic-env.js
@@ -26,7 +26,10 @@ const ENV_VARS_MAPPING = {
   CATENAX_PORTAL_API_URL: 'apiUrl',
   CATENAX_PORTAL_BASE_URL: 'baseUrl',
   CATENAX_PORTAL_URL: 'portalUrl',
-  GIT_TAG: 'gitTag'
+  GIT_TAG: 'gitTag',
+  BPN: 'bpn',
+  IMPRINT_URL: 'imprintPath',
+  PRIVACY_URL: 'privacyPath'
 };
 
 const ROOT_DIR = '/usr/share/nginx/html';
