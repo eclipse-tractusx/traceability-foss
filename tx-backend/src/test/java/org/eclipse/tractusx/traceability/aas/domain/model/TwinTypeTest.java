8d99f95ad47b165127ac3f2c59ed3a8f931b9eac
ds-vlisenco <Vladimir.Lisenco@doubleslash.de>
2025-04-30 15:37:47 +0200

    Push

---
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/aas/domain/model/TwinTypeTest.java
@@ -0,0 +1,34 @@
+package org.eclipse.tractusx.traceability.aas.domain.model;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwinTypeTest {
+
+    @Test
+    void testGetValue() {
+        assertEquals("PartType", TwinType.PART_TYPE.getValue());
+        assertEquals("PartInstance", TwinType.PART_INSTANCE.getValue());
+    }
+
+    @Test
+    void testEnumNames() {
+        assertEquals("PART_TYPE", TwinType.PART_TYPE.name());
+        assertEquals("PART_INSTANCE", TwinType.PART_INSTANCE.name());
+    }
+
+    @Test
+    void testEnumValueOf() {
+        assertEquals(TwinType.PART_TYPE, TwinType.valueOf("PART_TYPE"));
+        assertEquals(TwinType.PART_INSTANCE, TwinType.valueOf("PART_INSTANCE"));
+    }
+
+    @Test
+    void testNotNullValues() {
+        for (TwinType type : TwinType.values()) {
+            assertNotNull(type.getValue());
+        }
+    }
+}
