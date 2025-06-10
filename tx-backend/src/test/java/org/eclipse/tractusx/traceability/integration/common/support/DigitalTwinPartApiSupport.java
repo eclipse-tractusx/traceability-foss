ad1861dec667bd9d17e19b2a3910300eb6502256
ds-vlisenco <Vladimir.Lisenco@doubleslash.de>
2025-06-10 13:43:50 +0200

    feature/digital-twin-part

---
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/common/support/DigitalTwinPartApiSupport.java
@@ -0,0 +1,93 @@
+/********************************************************************************
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+package org.eclipse.tractusx.traceability.integration.common.support;
+
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.tractusx.traceability.common.security.JwtRole;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DigitalTwinPartApiSupport {
+
+    private final OAuth2Support oAuth2Support;
+    private static final JwtRole ROLE = JwtRole.ADMIN;
+
+    /**
+     * Executes POST request to /api/administration/digitalTwinPart endpoint
+     */
+    public <T> T postDigitalTwinPart(Object requestBody, int expectedStatusCode, TypeRef<T> typeRef) {
+        return RestAssured
+                .given()
+                .header(oAuth2Support.jwtAuthorization(ROLE))
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(requestBody)
+                .when()
+                .post("/api/administration/digitalTwinPart")
+                .then()
+                .log().all()
+                .statusCode(expectedStatusCode)
+                .extract()
+                .body()
+                .as(typeRef);
+    }
+
+    /**
+     * Executes POST request to /api/administration/digitalTwinPart/detail endpoint
+     */
+    public <T> T postDigitalTwinPartDetail(Object requestBody, int expectedStatusCode, Class<T> clazz) {
+        return RestAssured
+                .given()
+                .header(oAuth2Support.jwtAuthorization(ROLE))
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(requestBody)
+                .when()
+                .post("/api/administration/digitalTwinPart/detail")
+                .then()
+                .log().all()
+                .statusCode(expectedStatusCode)
+                .extract()
+                .as(clazz);
+    }
+
+    /**
+     * Executes POST request to /api/administration/digitalTwinPart/searchable-values endpoint
+     */
+    public <T> T postSearchableValues(Object requestBody, int expectedStatusCode, TypeRef<T> typeRef) {
+        return RestAssured
+                .given()
+                .header(oAuth2Support.jwtAuthorization(ROLE))
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(requestBody)
+                .when()
+                .post("/api/administration/digitalTwinPart/searchable-values")
+                .then()
+                .log().all()
+                .statusCode(expectedStatusCode)
+                .extract()
+                .body()
+                .as(typeRef);
+    }
+}
--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/digitaltwinpart/DigitalTwinPartsControllerIT.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/digitaltwinpart/DigitalTwinPartsControllerIT.java
@@ -9,9 +9,8 @@
  * https://www.apache.org/licenses/LICENSE-2.0.
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations
  * under the License.
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -29,7 +28,6 @@ import digitaltwinpart.DigitalTwinPartRequest;
 import digitaltwinpart.DigitalTwinPartResponse;
 import digitaltwinpart.SearchableDigitalTwinPartRequest;
 import io.restassured.common.mapper.TypeRef;
-import io.restassured.http.ContentType;
 import org.eclipse.tractusx.traceability.common.model.PageResult;
 import org.eclipse.tractusx.traceability.common.model.SearchCriteriaStrategy;
 import org.eclipse.tractusx.traceability.configuration.infrastructure.model.TriggerConfigurationEntity;
@@ -37,6 +35,7 @@ import org.eclipse.tractusx.traceability.integration.IntegrationTestSpecificatio
 import org.eclipse.tractusx.traceability.integration.common.support.AASDatabaseSupport;
 import org.eclipse.tractusx.traceability.integration.common.support.AssetsSupport;
 import org.eclipse.tractusx.traceability.integration.common.support.ConfigurationSupport;
+import org.eclipse.tractusx.traceability.integration.common.support.DigitalTwinPartApiSupport;
 import org.eclipse.tractusx.traceability.integration.common.support.EdcSupport;
 import org.eclipse.tractusx.traceability.integration.common.support.IrsApiSupport;
 import org.jose4j.lang.JoseException;
@@ -46,9 +45,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.tractusx.traceability.common.security.JwtRole.ADMIN;
 
 class DigitalTwinPartsControllerIT extends IntegrationTestSpecification {
 
@@ -60,9 +57,10 @@ class DigitalTwinPartsControllerIT extends IntegrationTestSpecification {
     IrsApiSupport irsApiSupport;
     @Autowired
     AASDatabaseSupport aasDatabaseSupport;
-
     @Autowired
     ConfigurationSupport configurationSupport;
+    @Autowired
+    DigitalTwinPartApiSupport digitalTwinPartApiSupport;
 
     @BeforeEach
     void setUp() throws JsonProcessingException {
@@ -71,8 +69,8 @@ class DigitalTwinPartsControllerIT extends IntegrationTestSpecification {
     }
 
     @Test
-    void shouldReturnDigitalTwinPartSorted() throws JoseException {
-        //GIVEN
+    void shouldReturnDigitalTwinPartSorted() {
+        // GIVEN
         assetsSupport.defaultAssetsStored();
         String aasId = "ABC_1";
         String aasId2 = "ABC_2";
@@ -86,27 +84,18 @@ class DigitalTwinPartsControllerIT extends IntegrationTestSpecification {
                 .sort(List.of("aasId,desc"))
                 .build();
 
-        //WHEN
-        PageResult<DigitalTwinPartResponse> digitalTwinPartResponsePageResult = given()
-                .header(oAuth2Support.jwtAuthorization(ADMIN))
-                .contentType(ContentType.JSON)
-                .log().all()
-                .when()
-                .body(digitalTwinPartRequest)
-                .post("/api/administration/digitalTwinPart")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().body().as(new TypeRef<>() {
-                });
-        //THEN
+        // WHEN
+        PageResult<DigitalTwinPartResponse> digitalTwinPartResponsePageResult =
+                digitalTwinPartApiSupport.postDigitalTwinPart(digitalTwinPartRequest, 200, new TypeRef<>() {});
+
+        // THEN
         assertThat(digitalTwinPartResponsePageResult.content()).hasSize(2);
         assertThat(digitalTwinPartResponsePageResult.content().get(0).getAasId()).isEqualTo(aasId2);
     }
 
     @Test
-    void shouldReturnDigitalTwinPartFiltered() throws JoseException {
-        //GIVEN
+    void shouldReturnDigitalTwinPartFiltered() {
+        // GIVEN
         assetsSupport.defaultAssetsStored();
         String aasId = "ABC_1";
         String aasId2 = "ABC_2";
@@ -124,6 +113,7 @@ class DigitalTwinPartsControllerIT extends IntegrationTestSpecification {
         DigitalTwinPartFilter digitalTwinPartFilter = DigitalTwinPartFilter.builder()
                 .aasId(filterAttribute)
                 .build();
+
         DigitalTwinPartRequest digitalTwinPartRequest = DigitalTwinPartRequest.builder()
                 .filters(List.of(digitalTwinPartFilter))
                 .page(0)
@@ -131,32 +121,24 @@ class DigitalTwinPartsControllerIT extends IntegrationTestSpecification {
                 .sort(List.of("aasId,desc"))
                 .build();
 
-        //WHEN
-        PageResult<DigitalTwinPartResponse> digitalTwinPartResponsePageResult = given()
-                .header(oAuth2Support.jwtAuthorization(ADMIN))
-                .contentType(ContentType.JSON)
-                .log().all()
-                .when()
-                .body(digitalTwinPartRequest)
-                .post("/api/administration/digitalTwinPart")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().body().as(new TypeRef<>() {
-                });
-        //THEN
+        // WHEN
+        PageResult<DigitalTwinPartResponse> digitalTwinPartResponsePageResult =
+                digitalTwinPartApiSupport.postDigitalTwinPart(digitalTwinPartRequest, 200, new TypeRef<>() {});
+
+        // THEN
         assertThat(digitalTwinPartResponsePageResult.content()).isNotEmpty();
         assertThat(digitalTwinPartResponsePageResult.content().get(0).getAasId()).isEqualTo(aasId);
     }
 
     @Test
-    void shouldReturnDigitalTwinPartDetails() throws JoseException {
-        //GIVEN
+    void shouldReturnDigitalTwinPartDetails() {
+        // GIVEN
         assetsSupport.defaultAssetsStored();
         String aasId = "ABC_1";
         String aasId2 = "ABC_2";
         aasDatabaseSupport.createAASEntityByAASId(aasId);
         aasDatabaseSupport.createAASEntityByAASId(aasId2);
+
         TriggerConfigurationEntity triggerConfigurationEntity = TriggerConfigurationEntity.builder()
                 .cronExpressionRegisterOrderTTLReached("* * * * * *")
                 .cronExpressionMapCompletedOrders("* * * * * *")
@@ -169,53 +151,33 @@ class DigitalTwinPartsControllerIT extends IntegrationTestSpecification {
                 .aasId(aasId)
                 .build();
 
-        //WHEN
-        DigitalTwinPartDetailResponse response = given()
-                .header(oAuth2Support.jwtAuthorization(ADMIN))
-                .contentType(ContentType.JSON)
-                .log().all()
-                .body(digitalTwinPartDetailRequest)
-                .when()
-                .post("/api/administration/digitalTwinPart/detail")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().as(DigitalTwinPartDetailResponse.class);
-        //THEN
+        // WHEN
+        DigitalTwinPartDetailResponse response =
+                digitalTwinPartApiSupport.postDigitalTwinPartDetail(digitalTwinPartDetailRequest, 200, DigitalTwinPartDetailResponse.class);
+
+        // THEN
         assertThat(response.getAasId()).isEqualTo(aasId);
         assertThat(response.getAasTTL()).isEqualTo(1000);
         assertThat(response.getActor()).isEqualTo(ActorResponse.SYSTEM);
         assertThat(response.getBpn()).isEqualTo("BPNL00000001TEST");
         assertThat(response.getDigitalTwinType()).isEqualTo("PART_TYPE");
-
     }
 
     @Test
-    void shouldReturnSearchableValuesForFieldName() throws JoseException {
+    void shouldReturnSearchableValuesForFieldName() {
         // GIVEN
         assetsSupport.defaultAssetsStored();
         String aasId = "ABC_FILTER_TEST";
         aasDatabaseSupport.createAASEntityByAASId(aasId);
 
-        var request = new SearchableDigitalTwinPartRequest();
+        SearchableDigitalTwinPartRequest request = new SearchableDigitalTwinPartRequest();
         request.setFieldName("aasId");
         request.setStartWith("ABC");
         request.setSize(10);
 
         // WHEN
-        List<String> results = given()
-                .header(oAuth2Support.jwtAuthorization(ADMIN))
-                .contentType(ContentType.JSON)
-                .log().all()
-                .body(request)
-                .when()
-                .post("/api/administration/digitalTwinPart/searchable-values")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract()
-                .body()
-                .as(new TypeRef<>() {});
+        List<String> results =
+                digitalTwinPartApiSupport.postSearchableValues(request, 200, new TypeRef<>() {});
 
         // THEN
         assertThat(results)
