aa8f012e5a871661268bed791165afa58fd25ff8
Stephan Bauer <stephan.bauer@catena-x.net>
2026-02-04 12:57:33 +0100

    chore: update template syntax to use new conditional directives and upgrade Angular dependencies to version 21.1.x

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -19,7 +19,7 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { CoreModule } from '@core/core.module';
 import { environment } from '@env';
@@ -39,6 +39,6 @@ if (window['Zone']) {
 
 function bootstrap() {
   platformBrowserDynamic()
-    .bootstrapModule(CoreModule)
+    .bootstrapModule(CoreModule, { applicationProviders: [provideZoneChangeDetection()], })
     .catch(err => console.error(err));
 }
